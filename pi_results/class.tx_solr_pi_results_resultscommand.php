f7c6c1c3287d7ac1ec3b2d429cd6ba42060e4beb
Ingo Renner <ingo@typo3.org>
2010-05-26 14:59:31 +0000

    Improved security by consistently using htmlentities() with ENT_QUOTES option
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@33524 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi_results/class.tx_solr_pi_results_resultscommand.php
+++ pi_results/class.tx_solr_pi_results_resultscommand.php
@@ -54,7 +54,7 @@ class tx_solr_pi_results_ResultsCommand implements tx_solr_Command {
 		$searchedFor = strtr(
 			$this->parentPlugin->pi_getLL('results_searched_for'),
 			array(
-				'@searchWord' => htmlspecialchars(trim($this->parentPlugin->piVars['q']))
+				'@searchWord' => htmlentities(trim($this->parentPlugin->piVars['q']), ENT_QUOTES)
 			)
 		);
 
