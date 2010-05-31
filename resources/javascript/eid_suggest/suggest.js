654f28d97e3697c3170a75680cdc315c6da13fd3
Ingo Renner <ingo@typo3.org>
2010-05-31 17:16:47 +0000

    fixing indentation
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@33759 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/javascript/eid_suggest/suggest.js
+++ resources/javascript/eid_suggest/suggest.js
@@ -1,34 +1,36 @@
 $(document).ready(function(){
 
-	$("#tx-solr-q").autocomplete( tx_solr_suggestUrl,
-	{
-		width: 300,
-		scroll: false,
-		dataType: "json",
-		minChars: 3,
+	$("#tx-solr-q").autocomplete(
+		tx_solr_suggestUrl,
+		{
+			width: 300,
+			scroll: false,
+			dataType: "json",
+			minChars: 3,
 
-		formatItem: function(data, i, max, value, term) {
-			return value;
-		},
+			formatItem: function(data, i, max, value, term) {
+				return value;
+			},
 
-		parse: function(data) {
-			var rs = new Array();
-			var output = new Array();
+			parse: function(data) {
+				var rs = new Array();
+				var output = new Array();
 
-			i = 0;
-			for (var term in data) {
-				output[i] = {
-					data   : term,
-					value  : term + ' (' + data[term] + ')',
-					result : term,
-					count  : data[term]
-				};
-				i++;
+				i = 0;
+				for (var term in data) {
+					output[i] = {
+						data   : term,
+						value  : term + ' (' + data[term] + ')',
+						result : term,
+						count  : data[term]
+					};
+					i++;
+				}
+
+				return output;
 			}
-
-			return output;
 		}
-	});
+	);
 
 }).result(function(event, data, formatted){
 	$("#tx-solr-search-form").submit();
