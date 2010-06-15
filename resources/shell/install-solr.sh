6f0c385ea3eb1b8b638e256b72fc31393b7c759e
Ingo Renner <ingo@typo3.org>
2010-06-15 15:57:07 +0000

    Updated Tomcat / Solr install script
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@34391 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/shell/install-solr.sh
+++ resources/shell/install-solr.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
+
+TOMCAT_VER=6.0.26
+SOLR_VER=1.4.0
+
 cd /opt
 mkdir solr-tomcat
 cd solr-tomcat/
-TOMCAT_VER=6.0.20
-wget http://mirrors.ibiblio.org/pub/mirrors/apache/tomcat/tomcat-6/v$TOMCAT_VER/bin/apache-tomcat-$TOMCAT_VER.zip
-#find the latest nightly solr build
-NIGHTLY=`wget -O - http://people.apache.org/builds/lucene/solr/nightly/ | grep '.zip' | sed 's/.*\(solr-20..-..-..\.zip\).*/\1/' | tail -1`
-wget http://people.apache.org/builds/lucene/solr/nightly/$NIGHTLY
+
+# Download Tomcat
+wget http://www.apache.org/dist/tomcat/tomcat-6/v$TOMCAT_VER/bin/apache-tomcat-$TOMCAT_VER.zip
+# Download Solr
+wget http://www.apache.org/dist/lucene/solr/$SOLR_VER/apache-solr-$SOLR_VER.zip
+
 unzip apache-tomcat-$TOMCAT_VER.zip
-unzip $NIGHTLY
-cp apache-solr-nightly/dist/apache-solr-nightly.war apache-tomcat-$TOMCAT_VER/webapps/solr.war
-cp -r apache-solr-nightly/example/solr .
+unzip apache-solr-$SOLR_VER.zip
+
+cp apache-solr-$SOLR_VER/dist/apache-solr-$SOLR_VER.war apache-tomcat-$TOMCAT_VER/webapps/solr.war
+cp -r apache-solr-$SOLR_VER/example/solr .
+
 chmod a+x apache-tomcat-$TOMCAT_VER/bin/*
 ./apache-tomcat-$TOMCAT_VER/bin/startup.sh
+
 echo "Now browse to http://localhost:8080/solr/admin/"
+
 #Note that the startup.sh script is run from the directory containing your solr home ./solr
 #since the solr webapp looks for $CWD/solr by default.
 #You can use JNDI or a System property to configure the solr home directory (described below)
