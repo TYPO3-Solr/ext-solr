c1ec1012cdd71f1464305a69705a4d706b0c969d
Ingo Renner <ingo@typo3.org>
2010-07-20 18:37:39 +0000

    Fixed issue #7327: Sorting component does not respect target page setting from flexform
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@35874 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi_results/class.tx_solr_pi_results_sortingcommand.php
+++ pi_results/class.tx_solr_pi_results_sortingcommand.php
@@ -69,6 +69,7 @@ class tx_solr_pi_results_SortingCommand implements tx_solr_Command {
 		$configuration = tx_solr_Util::getSolrConfiguration();
 		$configuredSortingFields = $configuration['search.']['sorting.']['fields.'];
 		$query = $this->search->getQuery();
+		$query->setLinkTargetPageId($this->parentPlugin->conf['search.']['targetPage']);
 		$sortingFields = array();
 
 		$urlParameters = t3lib_div::_GP('tx_solr');
