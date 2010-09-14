6a748c670144705972e0cbc1493a0db06f885a14
Ingo Renner <ingo@typo3.org>
2010-09-14 13:04:29 +0000

    whitespace change
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@38125 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/javascript/pi_results/results.js
+++ resources/javascript/pi_results/results.js
@@ -1,14 +1,16 @@
 jQuery(document).ready(function($){
-  $('.tx-solr-facet-hidden').hide();
-  $('a.tx-solr-facet-show-all').click(function() {
-	  if ($(this).parent().siblings('.tx-solr-facet-hidden:visible').length == 0) {
-	      $(this).parent().siblings('.tx-solr-facet-hidden').show();
-	      $(this).text(tx_solr_facetLabels.showFewer);
-	    }
-	    else {
-	      $(this).parent().siblings('.tx-solr-facet-hidden').hide();
-	      $(this).text(tx_solr_facetLabels.showMore);
-	    }
-	    return false;
-  }).appendTo($('#tx-solr-facets-available:has(.tx-solr-facet-hidden)'));
+
+	$('.tx-solr-facet-hidden').hide();
+	$('a.tx-solr-facet-show-all').click(function() {
+		if ($(this).parent().siblings('.tx-solr-facet-hidden:visible').length == 0) {
+			$(this).parent().siblings('.tx-solr-facet-hidden').show();
+			$(this).text(tx_solr_facetLabels.showFewer);
+		} else {
+			$(this).parent().siblings('.tx-solr-facet-hidden').hide();
+			$(this).text(tx_solr_facetLabels.showMore);
+		}
+
+		return false;
+	}).appendTo($('#tx-solr-facets-available:has(.tx-solr-facet-hidden)'));
+
 });
