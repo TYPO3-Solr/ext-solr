e66d5fab6d594a7ff311c6c017a7afc2c9b84216
Ingo Renner <ingo@typo3.org>
2010-09-14 15:28:45 +0000

    Added new interfaces to extend the functionality of EXT:solr
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@38153 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ interfaces/interface.tx_solr_resultdocumentmodifier.php
@@ -0,0 +1,48 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Ingo Renner <ingo@typo3.org>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+
+/**
+ * ResultDocumentModifier interface, allows to modify search result documents
+ *
+ * @author	Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage solr
+ */
+interface tx_solr_ResultDocumentModifier {
+
+	/**
+	 * Modifies the given query and returns the modified query as result
+	 *
+	 * @param	tx_solr_pi_results_ResultsCommand	The search result command
+	 * @return	array	The document with fields as array
+	 */
+	public function modifyResultDocument($resultCommand, array $resultDocument);
+
+}
+
+?>
