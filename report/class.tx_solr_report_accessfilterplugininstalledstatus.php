6edcfe9fa7b30541a729d522b9805f7372eb2f3c
Ingo Renner <ingo@typo3.org>
2010-09-29 09:01:38 +0000

    Changed the Access Filter Plugin status report to use Solr's plugin information request handler
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@38743 735d13b6-9817-0410-8766-e36946ffe9aa

--- report/class.tx_solr_report_accessfilterplugininstalledstatus.php
+++ report/class.tx_solr_report_accessfilterplugininstalledstatus.php
@@ -29,9 +29,26 @@
  *
  * @author	Ingo Renner <ingo@typo3.org>
  * @package TYPO3
- * @subpackage tx_solr
+ * @subpackage solr
  */
 class tx_solr_report_AccessFilterPluginInstalledStatus implements tx_reports_StatusProvider {
+
+	/**
+	 * Solr Access Filter plugin version.
+	 *
+	 * Must be updated when changing the plugin.
+	 *
+	 * @var	string
+	 */
+	const RECOMMENDED_PLUGIN_VERSION = '1.1.0';
+
+	/**
+	 * The plugin's Java class name.
+	 *
+	 * @var	string
+	 */
+	const PLUGIN_CLASS_NAME = 'org.typo3.solr.search.AccessFilterQParserPlugin';
+
 
 	/**
 	 * Compiles a collection of solrconfig.xml checks against each configured
@@ -45,36 +62,83 @@ class tx_solr_report_AccessFilterPluginInstalledStatus implements tx_reports_Sta
 		$solrConnections = t3lib_div::makeInstance('tx_solr_ConnectionManager')->getAllConnections();
 
 		foreach ($solrConnections as $solrConnection) {
+			if ($solrConnection->ping()) {
+				$installationStatus = $this->checkPluginInstallionStatus($solrConnection);
+				$versionStatus      = $this->checkPluginVersion($solrConnection);
 
-			if ($solrConnection->ping()
-			&& !$this->isAccessFilterQueryParserPluginInstalled($solrConnection)) {
+				if (!is_null($installationStatus)) {
+					$reports[] = $installationStatus;
+				}
 
-				$message = '<p style="margin-bottom: 10px;">EXT:solr comes with
-				a plugin for the Apache Solr server to ensure TYPO3 access
-				restrictions are enforced for search results, thus allowing
-				visitors to only see results they are allowed to see. The plugin
-				is not installed on the following server. It is
-				recommended to install the plugin.</p>';
-
-				$message .= '<p>Affected Solr server:</p>
-					<ul>'
-					. '<li>Host: ' . $solrConnection->getHost() . '</li>'
-					. '<li>Port: ' . $solrConnection->getPort() . '</li>'
-					. '<li>Path: ' . $solrConnection->getPath() . '</li>
-					</ul>';
-
-				$status = t3lib_div::makeInstance('tx_reports_reports_status_Status',
-					'Access Filter Query Parser Plugin',
-					'Not Installed',
-					$message,
-					tx_reports_reports_status_Status::WARNING
-				);
-
-				$reports[] = $status;
+				if (!is_null($versionStatus)) {
+					$reports[] = $versionStatus;
+				}
 			}
 		}
 
 		return $reports;
+	}
+
+	protected function checkPluginInstallionStatus(tx_solr_SolrService $solrConnection) {
+		$status = null;
+
+		if (!$this->isPluginInstalled($solrConnection)) {
+			$message = '<p style="margin-bottom: 10px;">
+			EXT:solr comes with a plugin for the Apache Solr server to
+			ensure TYPO3 access restrictions are enforced for search
+			results, thus allowing visitors to only see results they are
+			allowed to see. The plugin is not installed on the following
+			server. It is recommended to install the plugin if you are using
+			frontend user group access restrictions for your site.</p>';
+
+			$message .= '<p>Affected Solr server:</p>
+				<ul>'
+				. '<li>Host: ' . $solrConnection->getHost() . '</li>'
+				. '<li>Port: ' . $solrConnection->getPort() . '</li>'
+				. '<li>Path: ' . $solrConnection->getPath() . '</li>
+				</ul>';
+
+			$status = t3lib_div::makeInstance('tx_reports_reports_status_Status',
+				'Access Filter Plugin',
+				'Not Installed',
+				$message,
+				tx_reports_reports_status_Status::WARNING
+			);
+		}
+
+		return $status;
+	}
+
+	protected function checkPluginVersion(tx_solr_SolrService $solrConnection) {
+		$status = null;
+
+		if ($this->isPluginInstalled($solrConnection)
+			&& $this->isPluginOutdated($solrConnection)
+		) {
+			$installedPluginVersion = $this->getInstalledPluginVersion($solrConnection);
+
+			$message = '<p style="margin-bottom: 10px;">The Solr Access Filter
+			Plugin you are using is outdated. You are using version <strong>' .
+			$installedPluginVersion . '</strong>, the currently recommended
+			version is <strong>' . self::RECOMMENDED_PLUGIN_VERSION .
+			'</strong>.</p>';
+
+			$message .= '<p>Affected Solr server:</p>
+				<ul>'
+				. '<li>Host: ' . $solrConnection->getHost() . '</li>'
+				. '<li>Port: ' . $solrConnection->getPort() . '</li>'
+				. '<li>Path: ' . $solrConnection->getPath() . '</li>
+				</ul>';
+
+			$status = t3lib_div::makeInstance('tx_reports_reports_status_Status',
+				'Access Filter Plugin',
+				'Outdated',
+				$message,
+				tx_reports_reports_status_Status::WARNING
+			);
+		}
+
+		return $status;
 	}
 
 	/**
@@ -84,25 +148,52 @@ class tx_solr_report_AccessFilterPluginInstalledStatus implements tx_reports_Sta
 	 * @param	tx_solr_SolrService	Solr connection to check for the plugin.
 	 * @return	boolean	True if the plugin is installed, false otherwise.
 	 */
-	protected function isAccessFilterQueryParserPluginInstalled(tx_solr_SolrService $solrConnection) {
+	protected function isPluginInstalled(tx_solr_SolrService $solrConnection) {
 		$accessFilterQueryParserPluginInstalled = false;
 
-		$solrconfigXmlUrl = $solrConnection->getScheme() . '://'
-			. $solrConnection->getHost() . ':' . $solrConnection->getPort()
-			. $solrConnection->getPath()
-			. 'admin/file/?file=solrconfig.xml';
+		$pluginsInformation = $solrConnection->getPluginsInformation();
 
-		$solrconfigXml = simplexml_load_file($solrconfigXmlUrl);
-
-		foreach ($solrconfigXml->queryParser as $queryParser) {
-			if ($queryParser['name'] == 'typo3access'
-			&& $queryParser['class'] == 'org.typo3.solr.search.AccessFilterQParserPlugin') {
-				$accessFilterQueryParserPluginInstalled = true;
-				break;
-			}
+		if (isset($pluginsInformation->plugins->OTHER->{self::PLUGIN_CLASS_NAME})) {
+			$accessFilterQueryParserPluginInstalled = true;
 		}
 
 		return $accessFilterQueryParserPluginInstalled;
+	}
+
+	/**
+	 * Checks whether the installed plugin is current.
+	 *
+	 *  @param	tx_solr_SolrService	Solr connection to check for the plugin.
+	 * @return	boolean	True if the plugin is outdated, false if it meets the current version recommendation.
+	 */
+	protected function isPluginOutdated(tx_solr_SolrService $solrConnection) {
+		$pluginVersion = $this->getInstalledPluginVersion($solrConnection);
+
+		$pluginVersionOutdated = version_compare(
+			$pluginVersion,
+			self::RECOMMENDED_PLUGIN_VERSION,
+			'<'
+		);
+
+		return $pluginVersionOutdated;
+	}
+
+	/**
+	 * Gets the version of the installed plugin.
+	 *
+	 * @param	tx_solr_SolrService	Solr connection to check for the plugin.
+	 * @return	string	The installed plugin's version number.
+	 */
+	public function getInstalledPluginVersion(tx_solr_SolrService $solrConnection) {
+		$version = '0.0.0';
+
+		$pluginsInformation = $solrConnection->getPluginsInformation();
+		$rawVersion = $pluginsInformation->plugins->OTHER->{self::PLUGIN_CLASS_NAME}->version;
+
+		$explodedRawVersion = explode('-', $rawVersion);
+		$version = $explodedRawVersion[0];
+
+		return $version;
 	}
 }
 
