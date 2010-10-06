a360e8b006861b11fe3e45368a2adc783e1a4281
Ingo Renner <ingo@typo3.org>
2010-10-06 16:42:37 +0000

    Fixed a missing hook registration resulting in user groups not being tracked and thus making protected results publicly visible
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@38902 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -30,6 +30,8 @@ t3lib_extMgm::addPItoST43(
 
 	// adding the indexer to the same hook that EXT:indexed_search would use
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageIndexing']['tx_solr_Indexer'] = 'EXT:solr/classes/class.tx_solr_indexer.php:tx_solr_Indexer';
+	// tracking FE user groups used to protect content on a page
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['postInit']['tx_solr_Indexer'] = 'EXT:solr/classes/class.tx_solr_indexer.php:&tx_solr_Indexer';
 
    # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
 
