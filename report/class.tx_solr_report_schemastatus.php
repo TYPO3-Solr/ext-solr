6f3bad7f375484967dd1453474e73caa696d756b
Markus Goldbach <markus.goldbach@dkd.de>
2010-11-24 12:48:30 +0000

    Changed Schema, remove ISOLatin mapping from type textSpell, after problems with umlauts in suggestion
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@40553 735d13b6-9817-0410-8766-e36946ffe9aa

--- report/class.tx_solr_report_schemastatus.php
+++ report/class.tx_solr_report_schemastatus.php
@@ -44,7 +44,7 @@ class tx_solr_report_SchemaStatus implements tx_reports_StatusProvider {
 	 *
 	 * @var	string
 	 */
-	const RECOMMENDED_SCHEMA_VERSION = 'tx_solr-1-5-0--20100923';
+	const RECOMMENDED_SCHEMA_VERSION = 'tx_solr-1-5-0--20101124';
 
 	/**
 	 * Compiles a collection of schema version checks against each configured
