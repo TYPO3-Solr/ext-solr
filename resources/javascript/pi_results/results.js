811307b81db50ef57fbf1085ea1c8171749d4cd5
Ingo Renner <ingo@typo3.org>
2011-02-22 12:57:21 +0000

    Fixed issue #12543: Facets links "Show more" not working (results page), credits Pierrick Caillon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@44014 735d13b6-9817-0410-8766-e36946ffe9aa

--- resources/javascript/pi_results/results.js
+++ resources/javascript/pi_results/results.js
@@ -1,16 +1,15 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function(){
 
-	$('.tx-solr-facet-hidden').hide();
-	$('a.tx-solr-facet-show-all').click(function() {
-		if ($(this).parent().siblings('.tx-solr-facet-hidden:visible').length == 0) {
-			$(this).parent().siblings('.tx-solr-facet-hidden').show();
-			$(this).text(tx_solr_facetLabels.showFewer);
+	jQuery('.tx-solr-facet-hidden').hide();
+	jQuery('a.tx-solr-facet-show-all').click(function() {
+		if (jQuery(this).parent().siblings('.tx-solr-facet-hidden:visible').length == 0) {
+			jQuery(this).parent().siblings('.tx-solr-facet-hidden').show();
+			jQuery(this).text(tx_solr_facetLabels.showFewer);
 		} else {
-			$(this).parent().siblings('.tx-solr-facet-hidden').hide();
-			$(this).text(tx_solr_facetLabels.showMore);
+			jQuery(this).parent().siblings('.tx-solr-facet-hidden').hide();
+			jQuery(this).text(tx_solr_facetLabels.showMore);
 		}
 
 		return false;
-	}).appendTo($('#tx-solr-facets-available:has(.tx-solr-facet-hidden)'));
-
+	});
 });
