fdebe9282d909513d88e9546a39978244a9a6425
Ingo Renner <ingo@typo3.org>
2011-08-05 11:25:57 +0000

    Changed the request handlers to use edismax
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@50601 735d13b6-9817-0410-8766-e36946ffe9aa

--- report/class.tx_solr_report_solrconfigstatus.php
+++ report/class.tx_solr_report_solrconfigstatus.php
@@ -44,7 +44,7 @@ class tx_solr_report_SolrconfigStatus implements tx_reports_StatusProvider {
 	 *
 	 * @var	string
 	 */
-	const RECOMMENDED_SOLRCONFIG_VERSION = 'tx_solr-1-7-0--20110727';
+	const RECOMMENDED_SOLRCONFIG_VERSION = 'tx_solr-2-5-0--20110805';
 
 	/**
 	 * Compiles a collection of solrconfig version checks against each configured
