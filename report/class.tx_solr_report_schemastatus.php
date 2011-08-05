a6fb55e3bb63791bf2800aeb164b040c52455ec2
Ingo Renner <ingo@typo3.org>
2011-08-05 11:34:41 +0000

    Changed the schema.xml files to use relative paths in xIncludes (supported since Apache Solr 3.1)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@50602 735d13b6-9817-0410-8766-e36946ffe9aa

--- report/class.tx_solr_report_schemastatus.php
+++ report/class.tx_solr_report_schemastatus.php
@@ -44,7 +44,7 @@ class tx_solr_report_SchemaStatus implements tx_reports_StatusProvider {
 	 *
 	 * @var	string
 	 */
-	const RECOMMENDED_SCHEMA_VERSION = 'tx_solr-1-7-0--20110324';
+	const RECOMMENDED_SCHEMA_VERSION = 'tx_solr-2-5-0--20110805';
 
 	/**
 	 * Compiles a collection of schema version checks against each configured
