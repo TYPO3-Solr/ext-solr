23ef61cd1af68c3c45bb7e37e4a10b347e9b50e3
Stefan Sprenger <stefan.sprenger@dkd.de>
2011-12-13 10:50:08 +0000

    Changed rendering of facet values to only split by the first ":" to allow the use of colons
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@55416 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi_results/class.tx_solr_pi_results_facetingcommand.php
+++ pi_results/class.tx_solr_pi_results_facetingcommand.php
@@ -132,7 +132,8 @@ class tx_solr_pi_results_FacetingCommand implements tx_solr_PluginCommand {
 
 		$facetsInUse = array();
 		foreach ($filterParameters as $filter) {
-			list($filterName, $filterValue) = explode(':', $filter);
+				// only split by the first ":" to allow the use of colons
+			list($filterName, $filterValue) = explode(':', $filter, 2);
 
 			$facetConfiguration = $this->configuration['search.']['faceting.']['facets.'][$filterName . '.'];
 
