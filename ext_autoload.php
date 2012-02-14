5c070f3074717d82f6d36ec9845439957376cf7a
Ingo Renner <ingo@typo3.org>
2012-02-14 15:30:23 +0000

    fixed order of interfaces
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@57788 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -158,8 +158,8 @@ return array(
 
 		// interfaces
 
-	'tx_solr_additionalpageindexer' => $extensionPath . 'interfaces/interface.tx_solr_additionalpageindexer.php',
 	'tx_solr_additionalindexqueueitemindexer' => $extensionPath . 'interfaces/interface.tx_solr_additionalindexqueueitemindexer.php',
+	'tx_solr_additionalpageindexer' => $extensionPath . 'interfaces/interface.tx_solr_additionalpageindexer.php',
 	'tx_solr_commandpluginaware' => $extensionPath . 'interfaces/interface.tx_solr_commandpluginaware.php',
 	'tx_solr_facetrenderer' => $extensionPath . 'interfaces/interface.tx_solr_facetrenderer.php',
 	'tx_solr_facetsmodifier' =>  $extensionPath . 'interfaces/interface.tx_solr_facetsmodifier.php',
