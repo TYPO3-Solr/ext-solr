566620cb552e185e64594e2719344eb92ccbe3ce
Ingo Renner <ingo@typo3.org>
2012-02-14 15:31:07 +0000

    Added EXT:scheduler as dependency
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/solr/trunk@57789 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,6 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'pagebrowse' => '',
+			'scheduler' => '',
 			'php' => '5.2.0-0.0.0',
 			'typo3' => '4.5.0-0.0.0',
 		),
