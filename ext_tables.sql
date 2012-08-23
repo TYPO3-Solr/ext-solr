c37d3cf34392d9ff573ace72dd801593f69a03d6
Ingo Renner <ingo@typo3.org>
2012-08-23 17:53:46 +0200

    [TASK] Add cache for frequent searches
    Change-Id: I98dc8acd72da715db75c803a8df7daafb0a9acf2

--- ext_tables.sql
+++ ext_tables.sql
@@ -80,3 +80,32 @@ CREATE TABLE tx_solr_indexqueue_indexing_property (
 	PRIMARY KEY (uid),
 	KEY item_id (item_id)
 ) ENGINE=InnoDB;
+
+
+#
+# Table structure for table 'tx_solr_cache'
+#
+
+CREATE TABLE tx_solr_cache (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	content mediumblob,
+	lifetime int(11) unsigned DEFAULT '0' NOT NULL,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier)
+) ENGINE=InnoDB;
+
+
+#
+# Table structure for table 'tx_solr_cache_tags'
+#
+CREATE TABLE tx_solr_cache_tags (
+	id int(11) unsigned NOT NULL auto_increment,
+	identifier varchar(250) DEFAULT '' NOT NULL,
+	tag varchar(250) DEFAULT '' NOT NULL,
+	PRIMARY KEY (id),
+	KEY cache_id (identifier),
+	KEY cache_tag (tag)
+) ENGINE=InnoDB;
+
