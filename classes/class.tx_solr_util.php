1599ff4e8e6d62d1148bffdf103f87e0f91e677a
Stefan Galinski <stefan.galinski@gmail.com>
2012-12-13 11:51:19 -0800

    [BUGFIX] Insufficient absRefPrefix support for non-page-records
    Fixes: #43556
    Releases: 2.9, 2.8
    
    Change-Id: I75d2450c60fecf1229a5ea1f3a8241ba7df071fc

--- classes/class.tx_solr_util.php
+++ classes/class.tx_solr_util.php
@@ -323,6 +323,7 @@ class tx_solr_Util {
 
 			$GLOBALS['TSFE']->settingLanguage();
 			$GLOBALS['TSFE']->newCObj();
+			$GLOBALS['TSFE']->absRefPrefix = ($GLOBALS['TSFE']->config['config']['absRefPrefix'] ? trim($GLOBALS['TSFE']->config['config']['absRefPrefix']) : '');
 
 			if ($useCache) {
 				$tsfeCache[$cacheId] = $GLOBALS['TSFE'];
