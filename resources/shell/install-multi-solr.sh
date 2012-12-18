cab6bd77cf77910ff187832d7d0eca470ba415c6
Hauke Meyer <meyer@visionconnect.de>
2012-12-17 16:26:43 -0800

    [BUGFIX] Wrong Apache Solr version loaded in multi install script
    Fixes: #44050
    Releases: 2.9, 2.8
    
    Change-Id: Ib55324ef83c17e4bc7beff2d46addd296ca71445

--- resources/shell/install-multi-solr.sh
+++ resources/shell/install-multi-solr.sh
@@ -9,7 +9,7 @@
 clear
 
 TOMCAT_VER=6.0.35
-DEFAULT_SOLR_VER=3.5.0
+DEFAULT_SOLR_VER=3.6.1
 EXT_SOLR_VER=2.9
 EXT_SOLR_PLUGIN_VER=1.2.0
 
@@ -249,8 +249,7 @@ for SOLR in ${SOLR_VER[*]}
 do
   cd /opt/solr-tomcat
   cecho "Downloading Apache Solr $SOLR" $green
-  wget --progress=bar:force http://www.typo3-solr.com/fileadmin/files/apache-solr-3.5.0.zip 2>&1 | progressfilt
-#  wget --progress=bar:force http://apache.osuosl.org/lucene/solr/$SOLR/apache-solr-$SOLR.zip 2>&1 | progressfilt
+  wget --progress=bar:force http://apache.osuosl.org/lucene/solr/$SOLR/apache-solr-$SOLR.zip 2>&1 | progressfilt
   cecho "Unpacking Apache Solr." $green
   unzip -q apache-solr-$SOLR.zip
   cp apache-solr-$SOLR/dist/apache-solr-$SOLR.war tomcat/webapps/solr-$SOLR.war
