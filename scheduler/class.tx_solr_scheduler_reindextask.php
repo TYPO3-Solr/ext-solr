9a06bac75ad628b67736bf7d0959c580743eb994
Ingo Renner <ingo@typo3.org>
2013-01-14 12:51:09 -0800

    [TASK] Merge Solr site selector field provider into re-index task
    Change-Id: I50c32c56c6f2965a3efb3aa6d4ad00d9657a2389

--- scheduler/class.tx_solr_scheduler_reindextask.php
+++ scheduler/class.tx_solr_scheduler_reindextask.php
@@ -2,7 +2,8 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2011-2012 Christoph Moeller <support@network-publishing.de>
+*  (c) 2011-2013 Christoph Moeller <support@network-publishing.de>
+*  (c) 2012-2013 Ingo Renner <ingo@typo3.org>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -31,7 +32,7 @@
  * @package TYPO3
  * @subpackage solr
  */
-class tx_solr_scheduler_ReIndexTask extends tx_scheduler_Task {
+class tx_solr_scheduler_ReIndexTask extends tx_scheduler_Task implements tx_scheduler_AdditionalFieldProvider {
 
 	/**
 	 * The site this task is supposed to initialize the index queue for.
@@ -106,6 +107,69 @@ class tx_solr_scheduler_ReIndexTask extends tx_scheduler_Task {
 		}
 
 		return $information;
+	}
+
+	/**
+	 * Used to define fields to provide the Solr server address when adding
+	 * or editing a task.
+	 *
+	 * @param	array					$taskInfo: reference to the array containing the info used in the add/edit form
+	 * @param	tx_scheduler_Task		$task: when editing, reference to the current task object. Null when adding.
+	 * @param	tx_scheduler_module1	$schedulerModule: reference to the calling object (Scheduler's BE module)
+	 * @return	array					Array containg all the information pertaining to the additional fields
+	 *									The array is multidimensional, keyed to the task class name and each field's id
+	 *									For each field it provides an associative sub-array with the following:
+	 */
+	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) {
+		if ($schedulerModule->CMD == 'add') {
+			$taskInfo['site'] = NULL;
+		}
+
+		if ($schedulerModule->CMD == 'edit') {
+			$taskInfo['site'] = $task->getSite();
+		}
+
+		$additionalFields = array(
+			'site' => array(
+				'code'     => tx_solr_Site::getAvailableSitesSelector('tx_scheduler[site]', $taskInfo['site']),
+				'label'    => 'LLL:EXT:solr/lang/locallang.xml:scheduler_field_site',
+				'cshKey'   => '',
+				'cshLabel' => ''
+			)
+		);
+
+		return $additionalFields;
+	}
+
+	/**
+	 * Checks any additional data that is relevant to this task. If the task
+	 * class is not relevant, the method is expected to return TRUE
+	 *
+	 * @param	array					$submittedData: reference to the array containing the data submitted by the user
+	 * @param	tx_scheduler_module1	$parentObject: reference to the calling object (Scheduler's BE module)
+	 * @return	boolean					True if validation was ok (or selected class is not relevant), FALSE otherwise
+	 */
+	public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $schedulerModule) {
+		$result = FALSE;
+
+			// validate site
+		$sites = tx_solr_Site::getAvailableSites();
+		if (array_key_exists($submittedData['site'], $sites)) {
+			$result = TRUE;
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Saves any additional input into the current task object if the task
+	 * class matches.
+	 *
+	 * @param	array				$submittedData: array containing the data submitted by the user
+	 * @param	tx_scheduler_Task	$task: reference to the current task object
+	 */
+	public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
+		$task->setSite(t3lib_div::makeInstance('tx_solr_Site', $submittedData['site']));
 	}
 }
 
