b36fa18bcd612a60d014c54cb44fe753049b8603
Ingo Renner <ingo@typo3.org>
2014-04-09 13:41:37 -0600

    Fix phpDoc

--- Classes/CommandResolver.php
+++ Classes/CommandResolver.php
@@ -26,26 +26,26 @@
 /**
  * command resolver
  *
- * @author	Ingo Renner <ingo@typo3.org>
- * @package	TYPO3
- * @subpackage	solr
+ * @author Ingo Renner <ingo@typo3.org>
+ * @package TYPO3
+ * @subpackage solr
  */
 class Tx_Solr_CommandResolver {
 
 	/**
-	 * A mapping of command names to caommand classes to use when executing plugins.
+	 * A mapping of command names to command classes to use when executing plugins.
 	 *
-	 * @var	array
+	 * @var array
 	 */
 	protected static $commands = array();
 
 	/**
 	 * Registers a command and its command class for several plugins
 	 *
-	 * @param	string	$plugins comma separated list of plugin names (without pi_ prefix)
-	 * @param	string	$commandName command name
-	 * @param	string	$commandClass name of the class implementing the command
-	 * @param	integer	Bitmask of which requirements need to be met for a command to be executed
+	 * @param string $plugins comma separated list of plugin names (without pi_ prefix)
+	 * @param string $commandName command name
+	 * @param string $commandClass name of the class implementing the command
+	 * @param integer $requirements Bitmask of which requirements need to be met for a command to be executed
 	 */
 	public static function registerPluginCommand($plugins, $commandName, $commandClass, $requirements = Tx_Solr_PluginCommand::REQUIREMENT_HAS_SEARCHED) {
 		if (!array_key_exists($commandName, self::$commands)) {
@@ -82,9 +82,9 @@ class Tx_Solr_CommandResolver {
 	/**
 	 * Gets the commands registered for a specific plugin.
 	 *
-	 * @param	string	Plugin name to get the registered commands for.
-	 * @param	integer	Bitmask required by commands to be registered for.
-	 * @return	array	An array of plugin command names registered
+	 * @param string $pluginName Plugin name to get the registered commands for.
+	 * @param integer $pluginStatus Bitmask required by commands to be registered for.
+	 * @return array An array of plugin command names registered
 	 */
 	public static function getPluginCommands($pluginName, $pluginStatus = Tx_Solr_PluginCommand::REQUIREMENT_NONE) {
 		$commands = array();
@@ -118,8 +118,8 @@ class Tx_Solr_CommandResolver {
 	 * Determines which bits are set as a requirement for the plugin commands
 	 * to be registered for.
 	 *
-	 * @param	integer	$bitmask Bitmask
-	 * @return	array	An array of integers - the bit positions set to 1
+	 * @param integer $bitmask Bitmask
+	 * @return array An array of integers - the bit positions set to 1
 	 */
 	protected static function getRequiredBits($bitmask) {
 		$requiredBits = array();
@@ -136,9 +136,10 @@ class Tx_Solr_CommandResolver {
 	/**
 	 * Creates an instance of a command class
 	 *
-	 * @param	string	command name
-	 * @param	object	parent object, most likely a plugin object
-	 * @return	Tx_Solr_PluginCommand the requested command if found, or NULL othwise
+	 * @param string $commandName command name
+	 * @param object $parent parent object, most likely a plugin object
+	 * @return Tx_Solr_PluginCommand the requested command if found, or NULL otherwise
+	 * @throws RuntimeException when a command fails to implement interface Tx_Solr_PluginCommand
 	 */
 	public function getCommand($commandName, $parent) {
 		$command = NULL;
