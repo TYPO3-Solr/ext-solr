4019f3bf3801482cdc23bf80dba821ccc527af0e
Ingo Renner <ingo@typo3.org>
2014-04-15 11:41:57 -0700

    Require PHP 5.3+

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'pagebrowse' => '',
 			'scheduler' => '',
-			'php' => '5.2.0-0.0.0',
+			'php' => '5.3.0-0.0.0',
 			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
