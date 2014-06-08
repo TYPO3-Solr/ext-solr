f917183ad4cd3b0b519c9d814a27eb3118f21d02
Ingo Renner <ingo@typo3.org>
2014-06-07 21:11:11 -0700

    Fix phpDoc for type hints

--- Lib/SolrPhpClient/Apache/Solr/Service.php
+++ Lib/SolrPhpClient/Apache/Solr/Service.php
@@ -479,7 +479,7 @@ class Apache_Solr_Service
 	/**
 	 * Get the current configured HTTP Transport
 	 *
-	 * @return HttpTransportInterface
+	 * @return Apache_Solr_HttpTransport_Interface
 	 */
 	public function getHttpTransport()
 	{
