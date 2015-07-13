25edbe444a2685f8e950dfbf4e674190ea0b15a3
Ingo Renner <ingo@typo3.org>
2015-07-12 20:49:28 -0700

    Require TYPO3 6.2.0+
    Change-Id: I7cebf87f76a90d29cfb5b817bcf9099749693fc7

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'pagebrowse' => '',
 			'scheduler' => '',
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '4.5.0-6.2.99',
+			'typo3' => '6.2.0-6.99.99',
 		),
 		'conflicts' => array(
 		),
