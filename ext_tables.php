181d9a4f61a95500d144abedbb5fd98c95d45561
Steffen Ritter <info@rs-websystems.de>
2015-07-15 13:53:13 +0200

    [BUGFIX] Fix casing issue with the status reports

--- ext_tables.php
+++ ext_tables.php
@@ -138,7 +138,7 @@ if (TYPO3_MODE == 'BE') {
 	// registering reports
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['tx_reports']['status']['providers']['solr'] = array(
 		'Tx_Solr_Report_SchemaStatus',
-		'Tx_Solr_Report_SolrconfigStatus',
+		'Tx_Solr_Report_SolrConfigStatus',
 		'Tx_Solr_Report_SolrConfigurationStatus',
 		'Tx_Solr_Report_SolrStatus',
 		'Tx_Solr_Report_SolrVersionStatus',
