69c8e172ac6592a497a5c7b02f8fdfed082a84ac
Ingo Renner <ingo.renner@gmail.com>
2015-07-17 20:55:26 -0700

    Merge pull request #38 from hnadler/master
    [BUGFIX] Fixed query.filter.__pageSections, which resulted in a fatal…
    Conflicts:
    	PiResults/Results.php
    
    Change-Id: Id34bde871e3e8884ec0f328aaac21a88556e0e0c

--- Classes/Query.php
+++ Classes/Query.php
@@ -621,7 +621,7 @@ class Tx_Solr_Query {
 	/**
 	 * Limits the query to certain page tree branches
 	 *
-	 * @param array $pageIds Comma-separated list of page IDs
+	 * @param string $pageIds Comma-separated list of page IDs
 	 */
 	public function setRootlineFilter($pageIds) {
 		$pageIds = t3lib_div::trimExplode(',', $pageIds);
