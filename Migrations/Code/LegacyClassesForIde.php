73b43a588e79843cf7b702c483561dc38e5bdda4
Ingo Renner <ingo@typo3.org>
2015-08-01 12:34:38 -0700

    Restore class loading compatibility for viewhelpers
    Change-Id: I542d6a85791e02571e0652487549c2bc057aa467

---
+++ Migrations/Code/LegacyClassesForIde.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @deprecated
+ */
+abstract class Tx_Solr_ViewHelper_AbstractSubpartViewHelper extends ApacheSolrForTypo3\Solr\ViewHelper\AbstractSubpartViewHelper {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Crop extends ApacheSolrForTypo3\Solr\ViewHelper\Crop {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_CurrentResultNumber extends ApacheSolrForTypo3\Solr\ViewHelper\CurrentResultNumber {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Date extends ApacheSolrForTypo3\Solr\ViewHelper\Date {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Facet extends ApacheSolrForTypo3\Solr\ViewHelper\Facet {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Link extends ApacheSolrForTypo3\Solr\ViewHelper\Link {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Lll extends ApacheSolrForTypo3\Solr\ViewHelper\Lll {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Multivalue extends ApacheSolrForTypo3\Solr\ViewHelper\Multivalue {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_OddEven extends ApacheSolrForTypo3\Solr\ViewHelper\OddEven {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Relevance extends ApacheSolrForTypo3\Solr\ViewHelper\Relevance {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_RelevanceBar extends ApacheSolrForTypo3\Solr\ViewHelper\RelevanceBar {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_SolrLink extends ApacheSolrForTypo3\Solr\ViewHelper\SolrLink {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_SortIndicator extends ApacheSolrForTypo3\Solr\ViewHelper\SortIndicator {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_SortUrl extends ApacheSolrForTypo3\Solr\ViewHelper\SortUrl {}
+
+/**
+ * @deprecated
+ */
+class Tx_Solr_ViewHelper_Ts extends ApacheSolrForTypo3\Solr\ViewHelper\Ts {}
+
+/**
+ * @deprecated
+ */
+interface Tx_Solr_ViewHelper extends ApacheSolrForTypo3\Solr\ViewHelper\ViewHelper {}
+
+/**
+ * @deprecated
+ */
+interface Tx_Solr_ViewHelperProvider extends ApacheSolrForTypo3\Solr\ViewHelper\ViewHelperProvider {}
