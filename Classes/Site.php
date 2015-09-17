e165fa145e358044cb5d6fc3d1af94fb89b3d407
Ingo Renner <ingo@typo3.org>
2015-09-17 13:07:32 -0700

    Remove unused import
    Change-Id: I89e118f210f58f180d80e1c8d8ee54dc167e61dc

--- Classes/Site.php
+++ Classes/Site.php
@@ -24,7 +24,6 @@ namespace ApacheSolrForTypo3\Solr;
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-use ApacheSolrForTypo3\Solr\Util;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
