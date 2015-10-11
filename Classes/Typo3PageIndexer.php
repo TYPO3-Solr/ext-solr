09fa58c5c6f6d5641e5582efc878091a269407af
Alexander Stehlik <alexander.stehlik@gmail.com>
2015-10-11 13:03:19 +0200

    [BUGFIX] Use mount point parameter for rootline generation
    The page indexer appends the current mount point parameter to the
    rootline field (seperated by comma).
    
    The PageUidToHierarchy field processor splits the field value
    by comma and uses the second parameter as mount point parameter
    for generating the rootline.

--- Classes/Typo3PageIndexer.php
+++ Classes/Typo3PageIndexer.php
@@ -254,7 +254,13 @@ class Typo3PageIndexer {
 		$document->setField('typeNum',     $this->page->type);
 		$document->setField('created',     $pageRecord['crdate']);
 		$document->setField('changed',     $pageRecord['SYS_LASTCHANGED']);
-		$document->setField('rootline',    $this->page->id);
+
+		$rootline = $this->page->id;
+		$mountPointParameter = $this->getMountPointParameter();
+		if ($mountPointParameter !== '') {
+			$rootline .= ',' . $mountPointParameter;
+		}
+		$document->setField('rootline', $rootline);
 
 		// access
 		$document->setField('access', (string)$this->pageAccessRootline);
