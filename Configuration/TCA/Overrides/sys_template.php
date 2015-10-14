7f9c6290a9853417843709950893e7185eb8575e
Ingo Renner <ingo@typo3.org>
2015-10-14 16:54:10 +0200

    Tweak naming of TS template includes
    Change-Id: Ic88e4b1be172d136cd99c066cc2d39d0df6126d1

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,19 +1,19 @@
 <?php
 
 // TypoScript
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Solr/', 'Apache Solr - Default Configuration');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Solr/', 'Search - Base Configuration');
 
 // OpenSearch
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/OpenSearch/', 'Apache Solr - OpenSearch');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/OpenSearch/', 'Search - OpenSearch');
 
 // Extension Pre-Configuration
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueNews/', 'Apache Solr - Index Queue Configuration for news');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueNewsContentElements/', 'Apache Solr - Index Queue Configuration for news with content elements');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueTtNews/', 'Apache Solr - Index Queue Configuration for tt_news');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueNews/', 'Search - Index Queue Configuration for news');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueNewsContentElements/', 'Search - Index Queue Configuration for news with content elements');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IndexQueueTtNews/', 'Search - Index Queue Configuration for tt_news');
 
 // Examples
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/BoostQueries/', 'Apache Solr Example - Boost more recent results');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/EverythingOn/', 'Apache Solr Example - Everything On');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/FilterPages/', 'Apache Solr Example - Filter to only show page results');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IntroPackageSearchBox/', 'Apache Solr Example - Replace Introduction Package search box');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/BoostQueries/', 'Search - (Example) Boost more recent results');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/EverythingOn/', 'Search - (Example) Everything On');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/FilterPages/', 'Search - (Example) Filter to only show page results');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr', 'Configuration/TypoScript/Examples/IntroPackageSearchBox/', 'Search - (Example) Replace Introduction Package search box');
 
