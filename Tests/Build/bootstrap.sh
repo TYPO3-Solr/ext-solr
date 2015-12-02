3630a07f48c98577644fd4876e156e74ee84a8a8
Ingo Renner <ingo@typo3.org>
2015-12-01 22:11:50 -0800

    Install/Require php-cs-fixer through composer
    Change-Id: Icbd21feb6d8b8a56f4e89971deaa2ef0e7015fc4

--- Tests/Build/bootstrap.sh
+++ Tests/Build/bootstrap.sh
@@ -15,16 +15,13 @@ fi
 
 
 
-mkdir -p $SCRIPTPATH/.Build/bin
-wget http://cs.sensiolabs.org/get/php-cs-fixer.phar -O $SCRIPTPATH/.Build/bin/php-cs-fixer.phar
-wget https://phar.phpunit.de/phpunit-4.8.9.phar -O $SCRIPTPATH/.Build/bin/phpunit.phar
-
 if [[ $TYPO3_VERSION == ~6.2.* ]]; then
 	composer require --dev typo3/cms="$TYPO3_VERSION" typo3/cms-composer-installers="1.2.2 as 1.1.4"
 else
 	composer require --dev typo3/cms="$TYPO3_VERSION"
 fi
 
+# Restore composer.json
 git checkout composer.json
 
 export TYPO3_PATH_WEB=$SCRIPTPATH/.Build/Web
--- Tests/Build/cibuild.sh
+++ Tests/Build/cibuild.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
 echo "PWD: $(pwd)"
 
 export TYPO3_PATH_WEB=$(pwd)/.Build/Web
 
 echo "Check psr-2 compliance"
-output=$(php  $SCRIPTPATH/.Build/bin/php-cs-fixer.phar fix -v --dry-run Classes); if [[ $output ]]; then while read -r line; do echo -e "\e[00;31m$line\e[00m"; done <<< "$output"; fi;
+output=$(.Build/bin/php-cs-fixer fix -v --dry-run Classes);
+if [[ $output ]]; then
+    while read -r line; do
+        echo -e "\e[00;31m$line\e[00m";
+    done <<< "$output";
+fi
 
 echo "Run unit tests"
-php $SCRIPTPATH/.Build/bin/phpunit.phar --debug --colors -c Tests/Build/UnitTests.xml
+.Build/bin/phpunit --colors -c Tests/Build/UnitTests.xml
 
 echo "Run functional tests"
 export typo3DatabaseName="typo3";
@@ -17,4 +21,4 @@ export typo3DatabaseHost="localhost";
 export typo3DatabaseUsername="root";
 export typo3DatabasePassword="";
 
-php $SCRIPTPATH/.Build/bin/phpunit.phar --debug --colors -c Tests/Build/FunctionalTests.xml
+.Build/bin/phpunit --colors -c Tests/Build/FunctionalTests.xml
