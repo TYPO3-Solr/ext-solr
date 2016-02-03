e37e73c9e59e1e024277bd610c5acc8940ed8430
Timo Schmidt <timo.schmidt@dkd.de>
2016-02-03 18:02:08 +0100

    Use bootstrap modals for danger zone items and remove usage of ActionButtonViewHelper since this is possible with fluid core logic.

---
+++ Resources/Public/JavaScript/FormModal.js
@@ -0,0 +1,33 @@
+define([
+    'jquery',
+    'TYPO3/CMS/Backend/Modal'
+], function($, Modal) {
+    $(document).ready(function() {
+
+        $('.t3js-modal-formsubmit-trigger').click(function(){
+
+            var element = $(this);
+            var title = element.data('title');
+            var content = element.data('content');
+            var severity = (typeof top.TYPO3.Severity[element.data('severity')] !== 'undefined') ? top.TYPO3.Severity[element.data('severity')] : top.TYPO3.Severity.info;
+
+            Modal.confirm(title, content, severity, [
+                {
+                    text: 'Confirm',
+                    active: true,
+                    trigger: function() {
+                        element.parents('form:first').submit();
+                        Modal.dismiss();
+                    }
+                }, {
+                    text: 'Abort!',
+                    trigger: function() {
+                        Modal.dismiss();
+                    }
+                }
+            ]);
+
+            return false;
+        })
+    });
+});
