6aa2d3dff7b1a0690f3873e616c3bd546e8d74a3
Timo Schmidt <timo.schmidt@dkd.de>
2016-03-01 10:51:17 +0100

    Load Ext.grid.RowExander.js and include ExtDirectCode
    Fixes: #241

--- Classes/Backend/IndexInspector/IndexInspector.php
+++ Classes/Backend/IndexInspector/IndexInspector.php
@@ -112,10 +112,8 @@ class IndexInspector extends AbstractFunctionModule
             )
         );
 
-        $extJsExtensionCorePath = $this->document->backPath . '../t3lib/js/extjs/ux/';
-        $pageRenderer->addJsFile($extJsExtensionCorePath . 'Ext.grid.RowExpander.js');
-
-
+        $pageRenderer->addExtDirectCode(array('TYPO3.tx_solr.IndexInspector.Remote'));
+        $pageRenderer->addJsFile('sysext/backend/Resources/Public/JavaScript/extjs/ux/Ext.grid.RowExpander.js');
         $pageRenderer->addJsFile($this->document->backPath . $GLOBALS['PATHrel_solr'] . 'Resources/JavaScript/ExtJs/override/gridpanel.js');
         $pageRenderer->addJsFile($this->document->backPath . $GLOBALS['PATHrel_solr'] . 'Resources/JavaScript/ModIndex/index_inspector.js');
 
