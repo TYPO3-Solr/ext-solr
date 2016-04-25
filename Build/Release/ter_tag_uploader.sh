ba4b1913ce459ff561895ba491f8931fee383e72
Timo Schmidt <timo.schmidt@dkd.de>
2016-04-25 14:07:18 +0200

    * Create a symlink before upload to have the expected structure for determining the extesion key. * Use global upload script instead of composer dependency and do cleanup before upload

--- Build/Release/ter_tag_uploader.sh
+++ Build/Release/ter_tag_uploader.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+EXTENSION_KEY="solr"
 
 # This script is triggered by travis when a build has been triggered and was tagged
 #
 # See: http://insight.helhum.io/post/140850737265/automatically-upload-typo3-extensions-to-ter-with
 
-ls -l .Build/bin/
 echo "PWD: $(pwd)"
 echo "Travis tag is: ${TRAVIS_TAG}"
+
+export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASSWORD" ]; then
   php Build/Release/pre_upload_check.php
@@ -14,11 +16,18 @@ if [ -n "$TRAVIS_TAG" ] && [ -n "$TYPO3_ORG_USERNAME" ] && [ -n "$TYPO3_ORG_PASS
       echo -e "Preparing upload of release ${TRAVIS_TAG} to TER\n"
       curl -sSL https://raw.githubusercontent.com/alrra/travis-after-all/1.4.4/lib/travis-after-all.js | node
       if [ $? -eq 0 ]; then
-         # Cleanup before we upload
+         echo "Symlinking current folder to extension folder."
+         ln -s $(pwd) ../${EXTENSION_KEY}
+         cd ../${EXTENSION_KEY}/
+
          git reset --hard HEAD && git clean -fx
+         echo "Files in this package"
+
+         ls -l
+
          TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
          echo "Uploading release ${TRAVIS_TAG} to TER"
-         .Build/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+         upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
       fi;
    fi;
 else
