f30576c8edaa59feb9b0430113c56b8bb1024c09
Timo Schmidt <timo.hund@yahoo.com>
2016-08-17 11:14:52 +0200

    [TASK] Pin php-cs-fixer version to v1.11.7
    * Pinned version to 1.11.7
    * Make sure to use php-cs-fixer NOT as require dev dependency an do a global install in .travis.yml
    
    Fixes: #612

--- Build/Test/cibuild.sh
+++ Build/Test/cibuild.sh
@@ -14,10 +14,11 @@ ls -l .Build/bin/
 echo "Run PHP Lint"
 find . -name \*.php ! -path "./.Build/*" | parallel --gnu php -d display_errors=stderr -l {} > /dev/null \;
 
-.Build/bin/php-cs-fixer --version > /dev/null 2>&1
+# use from vendor dir
+php-cs-fixer --version > /dev/null 2>&1
 if [ $? -eq "0" ]; then
     echo "Check PSR-2 compliance"
-    .Build/bin/php-cs-fixer fix -v --level=psr2 --dry-run Classes
+    php-cs-fixer fix -v --level=psr2 --dry-run Classes
 
     if [ $? -ne "0" ]; then
         echo "Some files are not PSR-2 compliant"
