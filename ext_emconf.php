bcd43f670f55b935f416fa51dbfaec40fb8f1bec
Ingo Renner <ingo.renner@gmail.com>
2017-01-25 13:03:55 -0800

    Increase max supported version of TYPO3 to 8.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'scheduler' => '',
-            'typo3' => '7.6.0-8.9.99'
+            'typo3' => '7.6.0-8.99.99'
         ],
         'conflicts' => [],
         'suggests' => [
