b662a23e84308f3bc7329e863abdf70febdd4864
Timo Schmidt <timo.hund@yahoo.com>
2017-03-13 11:01:50 +0100

    [TASK] Add unit test for SiteRepository
    This PR:
    
    * Adds the unit tests for SiteRepository
    * Fixes a small in getAllLanguages
    
    Fixes: #1138

--- Tests/Unit/Domain/Site/SiteRepositoryTest.php
+++ Tests/Unit/Domain/Site/SiteRepositoryTest.php
@@ -24,10 +24,12 @@ namespace ApacheSolrForTypo3\Solr\Tests\Unit\Domain\Site;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use ApacheSolrForTypo3\Solr\Domain\Index\Queue\RecordMonitor\Helper\RootPageResolver;
 use ApacheSolrForTypo3\Solr\Domain\Site\SiteRepository;
 use ApacheSolrForTypo3\Solr\Site;
+use ApacheSolrForTypo3\Solr\System\Cache\TwoLevelCache;
 use ApacheSolrForTypo3\Solr\Tests\Unit\UnitTest;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Registry;
 
 /**
  * Testcase to check if the SiteRepository class works as expected.
@@ -35,16 +37,55 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * The unit test is used to make sure that the SiteRepository works as expected
  *
  * @author Thomas Hohn <tho@systime.dk>
+ * @author Timo Hund <timo.hund@dkd.de>
  */
 class SiteRepositoryTest extends UnitTest
 {
+    /**
+     * @var TwoLevelCache
+     */
+    protected $cacheMock;
+
+    /**
+     * @var RootPageResolver
+     */
+    protected $rootPageResolverMock;
+
+    /**
+     * @var Registry
+     */
+    protected $registryMock;
+
+    /**
+     * @var SiteRepository
+     */
+    protected $siteRepository;
+
+    public function setUp()
+    {
+        $this->cacheMock = $this->getDumbMock(TwoLevelCache::class);
+        $this->rootPageResolverMock = $this->getDumbMock(RootPageResolver::class);
+        $this->registryMock = $this->getDumbMock(Registry::class);
+
+        // we mock buildSite to avoid the creation of real Site objects and pass all dependencies as mock
+        $this->siteRepository = $this->getMockBuilder(SiteRepository::class)
+            ->setConstructorArgs([$this->rootPageResolverMock, $this->cacheMock, $this->registryMock])
+            ->setMethods(['buildSite'])
+            ->getMock();
+    }
+
     /**
      * @test
      */
     public function canGetSiteByRootPageId()
     {
-        // @todo
+        $this->fakeEmptyCache();
 
+        $this->assertThatSitesAreCreatedWithPageIds([4711]);
+        $this->assertCacheIsWritten();
+
+        $site = $this->siteRepository->getSiteByRootPageId(4711);
+        $this->assertInstanceOf(Site::class, $site);
     }
 
     /**
@@ -52,7 +93,14 @@ class SiteRepositoryTest extends UnitTest
      */
     public function canGetSiteByPageId()
     {
-        // @todo
+        $this->fakeEmptyCache();
+        $this->fakeExistingRootPage(222, 111);
+
+        $this->assertThatSitesAreCreatedWithPageIds([111]);
+        $this->assertCacheIsWritten();
+
+        $site = $this->siteRepository->getSiteByPageId(222);
+        $this->assertInstanceOf(Site::class, $site);
     }
 
     /**
@@ -60,7 +108,18 @@ class SiteRepositoryTest extends UnitTest
      */
     public function canGetFirstAvailableSite()
     {
-        // @todo
+        $this->fakeEmptyCache();
+        $this->fakeSitesInRegistry([
+            '333|0' => ['rootPageUid' => 333],
+            '333|1' => ['rootPageUid' => 333],
+            '333|2' => ['rootPageUid' => 333]
+        ]);
+
+        $this->assertThatSitesAreCreatedWithPageIds([333]);
+        $this->assertCacheIsWritten();
+
+        $site = $this->siteRepository->getFirstAvailableSite();
+        $this->assertInstanceOf(Site::class, $site);
     }
 
     /**
@@ -68,7 +127,19 @@ class SiteRepositoryTest extends UnitTest
      */
     public function canGetAvailableSites()
     {
-        // @todo
+        $this->fakeEmptyCache();
+        $this->fakeSitesInRegistry([
+            '123|0' => ['rootPageUid' => 123],
+            '123|1' => ['rootPageUid' => 123],
+            '234|0' => ['rootPageUid' => 234],
+            '234|2' => ['rootPageUid' => 234],
+        ]);
+
+        $this->assertThatSitesAreCreatedWithPageIds([123,234]);
+        $this->assertCacheIsWritten();
+
+        $sites = $this->siteRepository->getAvailableSites();
+        $this->assertCount(2, $sites, 'We expect to have two sites with two languages');
     }
 
     /**
@@ -76,6 +147,70 @@ class SiteRepositoryTest extends UnitTest
      */
     public function canGetAllLanguages()
     {
-        // @todo
+        $this->fakeEmptyCache();
+        $this->fakeSitesInRegistry([
+            '123|0' => ['rootPageUid' => 123],
+            '123|1' => ['rootPageUid' => 123],
+            '123|2' => ['rootPageUid' => 123],
+            '234|0' => ['rootPageUid' => 234]
+        ]);
+        $this->assertThatSitesAreCreatedWithPageIds([123,234]);
+
+        $siteOne = $this->siteRepository->getFirstAvailableSite();
+        $languages = $this->siteRepository->getAllLanguages($siteOne);
+        $this->assertEquals([0,1,2], $languages, 'Could not get languages for site');
     }
-}
+
+    /**
+     * @return void
+     */
+    protected function fakeEmptyCache()
+    {
+        $this->cacheMock->expects($this->any())->method('get')->will($this->returnValue(null));
+    }
+
+    /**
+     * @return void
+     */
+    protected function assertCacheIsWritten()
+    {
+        $this->cacheMock->expects($this->once())->method('set');
+    }
+
+    /**
+     * @param array $pageIds
+     */
+    protected function assertThatSitesAreCreatedWithPageIds(array $pageIds)
+    {
+        $this->siteRepository->expects($this->any())->method('buildSite')->will(
+            $this->returnCallback(function($idToUse) use ($pageIds) {
+                if(in_array($idToUse, $pageIds)) {
+                    $site = $this->getDumbMock(Site::class);
+                    $site->expects($this->any())->method('getRootPageId')->will(
+                        $this->returnValue($idToUse)
+                    );
+                    return $site;
+                }
+            })
+        );
+    }
+
+    /**
+     * @param integer $forPageId
+     * @param integer $rootPageId
+     */
+    protected function fakeExistingRootPage($forPageId, $rootPageId)
+    {
+        $this->rootPageResolverMock->expects($this->any())->method('getRootPageId')->with($forPageId)->will($this->returnValue($rootPageId));
+    }
+
+    /**
+     * @param array $sitesInRegistry
+     */
+    protected function fakeSitesInRegistry(array $sitesInRegistry)
+    {
+        $this->registryMock->expects($this->any())->method('get')->will(
+            $this->returnValue($sitesInRegistry)
+        );
+    }
+ }
