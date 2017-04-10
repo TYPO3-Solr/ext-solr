3501f7404cec26f1014a2828472f44cc572e605c
Rafael Kähm <rafael.kaehm@dkd.de>
2017-04-10 17:56:12 +0200

    [TASK] Extract sys_domain to SystemDomainRepository
    added SystemDomainRepository and extracted sql calls from SolrConfigurationStatus
    fixed Unit SolrConfigurationStatusTest with mocks
    
    Resolves: #1199

---
+++ Classes/System/Records/SystemDomain/SystemDomainRepository.php
@@ -0,0 +1,62 @@
+<?php
+namespace ApacheSolrForTypo3\Solr\System\Records\SystemDomain;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2010-2017 dkd Internet Service GmbH <solr-eb-support@dkd.de>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use ApacheSolrForTypo3\Solr\System\Records\AbstractRepository;
+
+class SystemDomainRepository extends AbstractRepository
+{
+    /**
+     * @var string
+     */
+    protected $table = 'sys_domain';
+
+    /**
+     * Retrieves sys_domain records for a set of root page ids.
+     *
+     * @param array $rootPageIds
+     * @return mixed
+     */
+    public function findDomainRecordsByRootPagesIds(array $rootPageIds = [])
+    {
+        $queryBuilder = $this->getQueryBuilder();
+        $resultTmp = $queryBuilder->select('uid', 'pid')
+            ->from($this->table)
+            ->where(
+                $queryBuilder->expr()->in('pid', $rootPageIds),
+                $queryBuilder->expr()->eq('redirectTo', '\'\'')
+            )->groupBy('uid', 'pid', 'sorting')
+            ->orderBy('pid')
+            ->addOrderBy('sorting')
+            ->execute()->fetchAll();
+
+        $result = [];
+        foreach ($resultTmp as $key => $row) {
+            $result[$row['pid']] = $row;
+        }
+        return $result;
+    }
+}
--- Tests/Unit/Report/SolrConfigurationStatusTest.php
+++ Tests/Unit/Report/SolrConfigurationStatusTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace ApacheSolrForTypo3\Solr\Test\Report;
+namespace ApacheSolrForTypo3\Solr\Tests\Unit\Report;
 
 /***************************************************************
  *  Copyright notice
@@ -25,6 +25,7 @@ namespace ApacheSolrForTypo3\Solr\Test\Report;
  ***************************************************************/
 
 use ApacheSolrForTypo3\Solr\Report\SolrConfigurationStatus;
+use ApacheSolrForTypo3\Solr\System\Records\SystemDomain\SystemDomainRepository;
 use ApacheSolrForTypo3\Solr\Tests\Unit\UnitTest;
 use TYPO3\CMS\Reports\Status;
 
@@ -42,9 +43,17 @@ class SolrConfigurationStatusTest extends UnitTest
      */
     protected $report;
 
+    /**
+     * @var SystemDomainRepository
+     */
+    protected $systemDomainRepository;
 
     public function setUp() {
         // we mock the methods to external dependencies.
+        $this->systemDomainRepository = $this->getMockBuilder(SystemDomainRepository::class)->setMethods(
+            ['findDomainRecordsByRootPagesIds']
+        )->getMock();
+
         $this->report = $this->getMockBuilder(SolrConfigurationStatus::class)->setMethods(
             [
                 'getDomainRecordsForRootPagesIds',
@@ -53,6 +62,9 @@ class SolrConfigurationStatusTest extends UnitTest
                 'getIsIndexingEnabled',
                 'initializeTSFE',
                 'getRenderedReport'
+            ]
+        )->setConstructorArgs([
+                'systemDomainRepository' => $this->systemDomainRepository
             ]
         )->getMock();
     }
@@ -69,6 +81,8 @@ class SolrConfigurationStatusTest extends UnitTest
 
         $this->report->expects($this->any())->method('getIsSolrEnabled')->will($this->returnValue(false));
         $this->report->expects($this->any())->method('getIsIndexingEnabled')->will($this->returnValue(false));
+
+        $this->systemDomainRepository->expects($this->any())->method('findDomainRecordsByRootPagesIds')->will($this->returnValue($fakedDomainRecords));
 
         // everything should be ok, so no report should be rendered
         $this->report->expects($this->never())->method('getRenderedReport');
@@ -94,6 +108,8 @@ class SolrConfigurationStatusTest extends UnitTest
             'SolrConfigurationStatusIndexing.html',
             ['pages' => [$fakedRootPages[1]]]
         )->will($this->returnValue('faked report output'));
+
+        $this->systemDomainRepository->expects($this->any())->method('findDomainRecordsByRootPagesIds')->will($this->returnValue($fakedDomainRecords));
 
         $states = $this->report->getStatus();
 
