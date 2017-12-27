2bca3b9064f744ae4d562c38cb2806269a2d7f2e
Timo Hund <timo.hund@yahoo.com>
2017-12-27 14:44:28 +0100

    [TASK] Update license to gpl version 3 (#1792)
    This pr:
    
    * Updates the license to the gpl version 3.0
    
    Fixes: #935

--- Classes/ViewHelpers/Widget/GroupItemPaginateViewHelper.php
+++ Classes/ViewHelpers/Widget/GroupItemPaginateViewHelper.php
@@ -10,7 +10,7 @@ namespace ApacheSolrForTypo3\Solr\ViewHelpers\Widget;
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  The GNU General Public License can be found at
