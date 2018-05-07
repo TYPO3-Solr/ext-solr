3b33ed0029cab06b180f43321e22910520427d71
Thomas Hohn <tho@systime.dk>
2018-05-07 14:40:48 +0200

    [BUGFIX] #1991 Added trim

--- Classes/System/Configuration/TypoScriptConfiguration.php
+++ Classes/System/Configuration/TypoScriptConfiguration.php
@@ -593,7 +593,7 @@ class TypoScriptConfiguration
             return '';
         }
 
-        return $initialPagesAdditionalWhereClause;
+        return trim($initialPagesAdditionalWhereClause);
     }
 
     /**
