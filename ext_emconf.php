c528a427ad9d11afffd7404ec423272ab277afe7
Timo Hund <timo.hund@yahoo.com>
2018-06-26 11:51:26 +0200

    [TASK] Allow to install with TYPO3 9.3 (#2020)
    This pr:
    
    * Allows to install with TYPO3 9.3
    * Just for development and testing TYPO3 9 is not officially supported yet.

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'scheduler' => '',
-            'typo3' => '8.7.0-9.0.99'
+            'typo3' => '8.7.0-9.3.99'
         ],
         'conflicts' => [],
         'suggests' => [
