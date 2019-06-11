f9bdd358f5c6704f5f1a417aec96db0f8c5bc30f
Stefan Froemken <froemken@gmail.com>
2019-06-11 16:19:10 +0200

    Correct indent in fixure sql file

--- Tests/Integration/Fixtures/fake_extension_table.sql
+++ Tests/Integration/Fixtures/fake_extension_table.sql
@@ -12,7 +12,7 @@ CREATE TABLE tx_fakeextension_domain_model_foo (
 	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
 	t3ver_count int(11) DEFAULT '0' NOT NULL,
 	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-  t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
 	t3_origuid int(11) DEFAULT '0' NOT NULL,
 	editlock tinyint(4) DEFAULT '0' NOT NULL,
 	sys_language_uid int(11) DEFAULT '0' NOT NULL,
