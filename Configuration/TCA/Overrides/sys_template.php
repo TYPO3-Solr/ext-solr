9d2dcbfc2e359a2a23383c86f9f52890141718d2
Timo Hund <timo.hund@yahoo.com>
2019-09-15 15:41:30 +0200

    [TASK] Label template "Use connections settings from TYPO3_CONF_VARS" as deprecated (#2410)
    This pull request:
    
    * Labels the TypoScript example template to use TYPO3_CONV_VARS as deprecated
    
    Fixes: #2405

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -38,7 +38,7 @@
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr',
     'Configuration/TypoScript/Examples/ConnectionFromConfVars/',
-    'Search - (Example) Use connection settings from TYPO3_CONF_VARS');
+    'Deprecated: Search - (Example) Use connection settings from TYPO3_CONF_VARS');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('solr',
     'Configuration/TypoScript/Examples/Suggest/',
     'Search - (Example) Suggest/autocomplete with jquery');
