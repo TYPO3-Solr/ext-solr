7940a51e042558e768877fc6467ced080c6cdfae
Rafael Kähm <rafael.kaehm@dkd.de>
2019-09-16 17:01:56 +0200

    [TASK] evaluate Integr./ConnectionManagerTest skipped test and reenable them
    * use default test site configuration for integration tests
    * implement tests for `getConnectionBy*`
    * replace `exceptionIsThrownForUnAvailableSolrConnectionOnGetConfigurationByRootPageId` with:
      * `exceptionIsThrownForUnAvailableSolrConnectionOnGetConnectionByRootPageId`
      * `exceptionIsThrownForUnAvailableSolrConnectionOnGetConnectionByPageId`
    
    Fixes: #2401

--- Tests/Integration/ConnectionManagerTest.php
+++ Tests/Integration/ConnectionManagerTest.php
@@ -27,7 +27,10 @@ namespace ApacheSolrForTypo3\Solr\Tests\Integration;
 use ApacheSolrForTypo3\Solr\ConnectionManager;
 use ApacheSolrForTypo3\Solr\NoSolrConnectionFoundException;
 use ApacheSolrForTypo3\Solr\System\Solr\SolrConnection;
+use Nimut\TestingFramework\Exception\Exception;
+use ReflectionException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use function vsprintf;
 
 /**
  * This testcase can be used to check if the ConnectionManager can be used
@@ -38,9 +41,195 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class ConnectionManagerTest extends IntegrationTest
 {
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->writeDefaultSolrTestSiteConfiguration();
+    }
+
+    /**
+     * @return array
+     */
+    public function  canFindSolrConnectionsByRootPageIdDataProvider()
+    {
+        return [
+            ['rootPageId' => 1, 'siteName' => 'integration_tree_one', 'expectedSolrHost' => 'solr.testone.endpoint'],
+            ['rootPageId' => 111, 'siteName' => 'integration_tree_two', 'expectedSolrHost' => 'solr.testtwo.endpoint']
+        ];
+    }
+
+    /**
+     * ConnectionManager can find connection by root page ID (1 and then 111).
+     * There is following scenario:
+     *
+     * [0]
+     *  |
+     *  ——[ 1] First site
+     *  |
+     *  ——[111] Second site
+     *
+     * @test
+     * @dataProvider canFindSolrConnectionsByRootPageIdDataProvider
+     *
+     * @param int $rootPageId
+     * @param string $siteName
+     * @param string $expectedSolrHost
+     * @throws NoSolrConnectionFoundException
+     * @throws Exception
+     * @throws ReflectionException
+     */
+    public function canFindSolrConnectionsByRootPageId(int $rootPageId, string $siteName, string $expectedSolrHost)
+    {
+        $this->mergeSiteConfiguration($siteName, ['solr_host_read' => $expectedSolrHost]);
+        $this->importDataSetFromFixture('ConnectionManagerTest_basic_connections.xml');
+
+        /** @var $connectionManager ConnectionManager */
+        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
+
+        foreach ([0,1,2] as $languageID) {
+            $solrService = $connectionManager->getConnectionByRootPageId($rootPageId, $languageID);
+            $this->assertInstanceOf(SolrConnection::class, $solrService, vsprintf('Should find solr connection for root page "%s" and language "%s"', [$rootPageId, $languageID]));
+            $this->assertEquals($expectedSolrHost, $solrService->getNode('read')->getHost(), vsprintf('Apache Solr host must be the same as configured.' .
+                ' Wrong connection is used. I expected "%s" as Host for "%s" Site with Root-Page ID "%".', [$expectedSolrHost, $siteName, $rootPageId]));
+        }
+    }
+
+
+    /**
+     * @return array
+     */
+    public function  canFindSolrConnectionsByPageIdDataProvider()
+    {
+        return [
+            ['pageId' => 11, 'siteName' => 'integration_tree_one', 'expectedSolrHost' => 'solr.testone.endpoint'],
+            ['ageId' => 21, 'siteName' => 'integration_tree_two', 'expectedSolrHost' => 'solr.testtwo.endpoint']
+        ];
+    }
+
+    /**
+     * The connection manager must find the connections for Root Pages 1 and 111,
+     * by some of page IDs in desired tree(connection for 1 by 11, for 111 by 21).
+     * There is following scenario:
+     *
+     * [0]
+     *  |
+     *  ——[ 1] First site
+     *  |   |
+     *  |   ——[11] Subpage of first site
+     *  |
+     *  ——[111] Second site
+     *  |  |
+     *  |  ——[21] Subpage of second site
+     *  |
+     *  ——[ 3] Detached and non Root Page-Tree
+     *      |
+     *      —— [31] Subpage 1 of Detached
+     *      |
+     *      —— [32] Subpage 2 of Detached
+     *
+     * @test
+     * @dataProvider canFindSolrConnectionsByPageIdDataProvider
+     *
+     * @param int $rootPageId
+     * @param string $siteName
+     * @param string $expectedSolrHost
+     * @throws NoSolrConnectionFoundException
+     * @throws Exception
+     * @throws ReflectionException
+     */
+    public function canFindSolrConnectionsByPageId(int $pageId, string $siteName, string $expectedSolrHost)
+    {
+        $this->mergeSiteConfiguration($siteName, ['solr_host_read' => $expectedSolrHost]);
+        $this->importDataSetFromFixture('ConnectionManagerTest_basic_connections.xml');
+
+        /** @var $connectionManager ConnectionManager */
+        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
+
+        foreach ([0,1,2] as $languageID) {
+            $solrService = $connectionManager->getConnectionByPageId($pageId, $languageID);
+            $this->assertInstanceOf(SolrConnection::class, $solrService, vsprintf('Should find solr connection for page id "%s" and language "%s"', [$pageId, $languageID]));
+            $this->assertEquals($expectedSolrHost, $solrService->getNode('read')->getHost(), vsprintf('Apache Solr host must be the same as configured.' .
+                ' Wrong connection is used. I expected "%s" as Host for "%s" Site with Root-Page ID "%".', [$expectedSolrHost, $siteName, $pageId]));
+        }
+    }
+
+    /**
+     * There is following scenario for setup, the remaining stuff happens in test methods.:
+     *
+     * [0]
+     *  |
+     *  ——[ 3] Detached and non Root Page-Tree
+     *      |
+     *      —— [31] Subpage 1 of Detached
+     *      |
+     *      —— [32] Subpage 2 of Detached
+     */
+    protected function setupNotFullyConfiguredSite()
+    {
+        $defaultLanguage = $this->buildDefaultLanguageConfiguration('EN', '/en/');
+        $german = $this->buildLanguageConfiguration('DE', '/de/');
+        $danish = $this->buildLanguageConfiguration('DA', '/da/');
+        $this->writeSiteConfiguration(
+            'integration_tree_three',
+            $this->buildSiteConfiguration(3, 'http://testthree.site/'),
+            [
+                $defaultLanguage, $german, $danish
+            ],
+            [
+                $this->buildErrorHandlingConfiguration('Fluid', [404])
+            ]
+        );
+    }
+
+    /**
+     * The connection manager must throw an exception on configured site without solr connection information by trying to get connection by root page id.
+     * There is following scenario:
+     *
+     * [0]
+     *  |
+     *  ——[ 3] Detached and non Root Page-Tree
+     *
+     * @test
+     */
+    public function exceptionIsThrownForUnAvailableSolrConnectionOnGetConnectionByRootPageId()
+    {
+        $this->setupNotFullyConfiguredSite();
+
+        $this->expectException(NoSolrConnectionFoundException::class);
+        /** @var $connectionManager ConnectionManager */
+        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
+        $connectionManager->getConnectionByRootPageId(3);
+
+        $this->expectException(NoSolrConnectionFoundException::class);
+        $connectionManager->getConnectionByPageId(31);
+    }
+
+    /**
+     * The connection manager must throw an exception on configured site without solr connection information by trying to get connection by page id.
+     * There is following scenario:
+     *
+     * [0]
+     *  |
+     *   ——[ 3] Detached and non Root Page-Tree
+     *       |
+     *       —— [31] Subpage 1 of Detached
+     *       |
+     *       —— [32] Subpage 2 of Detached
+     *
+     * @test
+     */
+    public function exceptionIsThrownForUnAvailableSolrConnectionOnGetConnectionByPageId()
+    {
+        $this->setupNotFullyConfiguredSite();
+
+        $this->expectException(NoSolrConnectionFoundException::class);
+        /** @var $connectionManager ConnectionManager */
+        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
+        $connectionManager->getConnectionByPageId(31);
+    }
+
     /**
      * ConnectionManager must use the connection for site(tree), where mount Point is defined.
-     *
      * There is following scenario:
      *
      *     [0]
@@ -61,8 +250,6 @@ class ConnectionManagerTest extends IntegrationTest
      */
     public function canFindSolrConnectionForMountedPageIfMountPointIsGiven()
     {
-        $this->markTestSkipped('Fixme');
-
         $this->importDataSetFromFixture('can_find_connection_for_mouted_page.xml');
 
         /** @var $connectionManager ConnectionManager */
@@ -73,31 +260,5 @@ class ConnectionManagerTest extends IntegrationTest
 
         $solrService1 = $connectionManager->getConnectionByPageId(25, 0, '24-14');
         $this->assertInstanceOf(SolrConnection::class, $solrService1, 'Should find solr connection for level 1 of mounted page.');
-    }
-
-    /**
-     * @test
-     */
-    public function exceptionIsThrownForUnAvailableSolrConnectionOnGetConfigurationByRootPageId()
-    {
-        $this->markTestSkipped('Fixme');
-
-        $this->expectException(NoSolrConnectionFoundException::class);
-
-        /** @var $connectionManager ConnectionManager */
-        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
-        $connectionManager->getConfigurationByRootPageId(4711);
-    }
-
-    /**
-     * @test
-     */
-    public function exceptionIsThrownForUnAvailableSolrConnectionOnGetConnectionByPageId()
-    {
-        $this->expectException(NoSolrConnectionFoundException::class);
-
-        /** @var $connectionManager ConnectionManager */
-        $connectionManager = GeneralUtility::makeInstance(ConnectionManager::class);
-        $connectionManager->getConnectionByPageId(888);
     }
 }
