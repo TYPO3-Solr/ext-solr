5a73e5d1f4faaee3c44c74b0e65ace1dbe63c64c
dkd-kaehm <rafael.kaehm@dkd.de>
2019-10-21 08:15:16 +0200

    [TASK] Site configuration status check - incl. languages config fallback (#2484)
    Fixes: #2480

--- Classes/Report/SiteHandlingStatus.php
+++ Classes/Report/SiteHandlingStatus.php
@@ -24,11 +24,15 @@ namespace ApacheSolrForTypo3\Solr\Report;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use ApacheSolrForTypo3\Solr\Domain\Site\Site;
 use ApacheSolrForTypo3\Solr\Domain\Site\SiteRepository;
 use ApacheSolrForTypo3\Solr\Domain\Site\Typo3ManagedSite;
 use ApacheSolrForTypo3\Solr\System\Configuration\ExtensionConfiguration;
+use ApacheSolrForTypo3\Solr\System\Url\UrlHelper;
 use Exception;
+use Psr\Http\Message\UriInterface;
 use TYPO3\CMS\Core\Site\Entity\Site as Typo3Site;
+use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Reports\Status;
 
@@ -42,6 +46,13 @@ use TYPO3\CMS\Reports\Status;
 class SiteHandlingStatus extends AbstractSolrStatus
 {
     const TITLE_SITE_HANDLING_CONFIGURATION = 'Site handling configuration';
+
+    const
+        CSS_STATUS_NOTICE = 'notice',
+        CSS_STATUS_INFO = 'info',
+        CSS_STATUS_OK = 'success',
+        CSS_STATUS_WARNING = 'warning',
+        CSS_STATUS_ERROR = 'danger';
 
     /**
      * Site Repository
@@ -78,13 +89,14 @@ class SiteHandlingStatus extends AbstractSolrStatus
             $reports[] = GeneralUtility::makeInstance(
                 Status::class,
                 /** @scrutinizer ignore-type */ self::TITLE_SITE_HANDLING_CONFIGURATION,
-                /** @scrutinizer ignore-type */ 'The lagacy site mode is enabled.',
+                /** @scrutinizer ignore-type */ 'The lagacy site mode is enabled. This setting is global and can not be applied per site.',
                 /** @scrutinizer ignore-type */ 'The legacy site mode is not recommended and will be removed in EXT:Solr 11. Please switch to site handling as soon as possible.',
                 /** @scrutinizer ignore-type */ Status::WARNING
             );
             return $reports;
         }
 
+        /* @var Site $site */
         foreach ($this->siteRepository->getAvailableSites() as $site) {
             if (!($site instanceof Typo3ManagedSite)) {
                 $reports[] = GeneralUtility::makeInstance(
@@ -94,10 +106,9 @@ class SiteHandlingStatus extends AbstractSolrStatus
                     /** @scrutinizer ignore-type */ vsprintf('The configured Site "%s" is not TYPO3 managed site. Please refer to TYPO3 site management docs and configure the site properly.', [$site->getLabel()]),
                     /** @scrutinizer ignore-type */ Status::ERROR
                 );
-                return $reports;
+                continue;
             }
-
-            $reports[] = $this->generateValidationReportForSiteHandlingConfiguration($site->getTypo3SiteObject());
+            $reports[] = $this->generateValidationReportForSingleSite($site->getTypo3SiteObject());
         }
 
         return $reports;
@@ -109,40 +120,89 @@ class SiteHandlingStatus extends AbstractSolrStatus
      * @param Typo3Site $ypo3Site
      * @return Status
      */
-    protected function generateValidationReportForSiteHandlingConfiguration(Typo3Site $ypo3Site): Status
+    protected function generateValidationReportForSingleSite(Typo3Site $ypo3Site): Status
     {
         $variables = [
             'identifier' => $ypo3Site->getIdentifier()
         ];
         $globalPassedStateForThisSite = true;
 
-        // check scheme of base URI
-        $schemeValidationStatus = !empty($ypo3Site->getBase()->getScheme()) && (strpos('http', $ypo3Site->getBase()->getScheme()) !== false);
-        $variables['validationResults']['scheme'] = [
-            'label' => 'Requirement: Valid scheme in Entry Point[base].',
-            'message' => 'Entry Point[base] must contain valid HTTP scheme as http[s]:// to be able to index records.',
-            'passed' => $schemeValidationStatus
-        ];
-        $globalPassedStateForThisSite = $globalPassedStateForThisSite && $schemeValidationStatus;
-
-        // check authority of base URI
-        $authorityValidationStatus = !empty($ypo3Site->getBase()->getAuthority());
-        $variables['validationResults']['authority'] = [
-            'label' => 'Requirement: Valid Authority in Entry Point[base].',
-            'message' => 'Entry Point[base] must define the authority as [user-info@]host[:port] to be able to index records.',
-            'passed' => $authorityValidationStatus
-        ];
-        $globalPassedStateForThisSite = $globalPassedStateForThisSite && $authorityValidationStatus;
+        foreach ($ypo3Site->getAllLanguages() as $siteLanguage) {
+            if (!$siteLanguage->isEnabled()) {
+                $variables['validationResults'][$siteLanguage->getTitle()] = [
+                    'label' => 'Language: ' . $siteLanguage->getTitle(),
+                    'message' => 'No checks: The language is disabled in site configuration.',
+                    'CSSClassesFor' => [
+                        'tr' => self::CSS_STATUS_NOTICE
+                    ],
+                    'passed' => true
+                ];
+                continue;
+            }
+            $variables['validationResults'][$siteLanguage->getTitle()] = $this->generateValidationResultsForSingleSiteLanguage($siteLanguage);
+            $globalPassedStateForThisSite = $globalPassedStateForThisSite && $variables['validationResults'][$siteLanguage->getTitle()]['passed'];
+        }
 
         $renderedReport = $this->getRenderedReport('SiteHandlingStatus.html', $variables);
         /* @var Status $status */
         $status = GeneralUtility::makeInstance(
             Status::class,
-            /** @scrutinizer ignore-type */ 'Site Identifier: ' . $ypo3Site->getIdentifier(),
+            /** @scrutinizer ignore-type */ sprintf('Site Identifier: "%s"', $ypo3Site->getIdentifier()),
             /** @scrutinizer ignore-type */ '',
             /** @scrutinizer ignore-type */ $renderedReport,
             /** @scrutinizer ignore-type */ $globalPassedStateForThisSite == true ? Status::OK : Status::ERROR
         );
         return $status;
+    }
+
+    /**
+     * Generates the validation result array for using them in standalone view as an table row.
+     *
+     * @param SiteLanguage $siteLanguage
+     * @return array
+     */
+    protected function generateValidationResultsForSingleSiteLanguage(SiteLanguage $siteLanguage): array
+    {
+        $validationResult = [
+            'label' => 'Language: ' . $siteLanguage->getTitle(),
+            'passed' => true,
+            'CSSClassesFor' => [
+                'tr' => self::CSS_STATUS_OK
+            ]
+        ];
+
+        if (!GeneralUtility::isValidUrl((string)$siteLanguage->getBase())) {
+            $validationResult['message'] = sprintf('Entry Point[base]="%s" is not valid URL. Following parts of defined URL are empty or invalid: "%s"', (string)$siteLanguage->getBase(), $this->fetchInvalidPartsOfUri($siteLanguage->getBase()));
+            $validationResult['passed'] = false;
+            $validationResult['CSSClassesFor']['tr'] = self::CSS_STATUS_ERROR;
+        } else {
+            $validationResult['message'] = sprintf('Entry Point[base]="%s" is valid URL.', (string)$siteLanguage->getBase());
+        }
+
+        return $validationResult;
+    }
+
+    /**
+     * @param UriInterface $uri
+     * @return string
+     */
+    protected function fetchInvalidPartsOfUri(UriInterface $uri): string
+    {
+        $invalidParts = '';
+        /* @var UrlHelper $solrUriHelper */
+        $solrUriHelper = GeneralUtility::makeInstance(UrlHelper::class, $uri);
+        try {
+            $solrUriHelper->getScheme();
+        } catch (\TypeError $error) {
+            $invalidParts .= 'scheme';
+        }
+
+        try {
+            $solrUriHelper->getHost();
+        } catch (\TypeError $error) {
+            $invalidParts .= ', host';
+        }
+
+        return $invalidParts;
     }
 }
