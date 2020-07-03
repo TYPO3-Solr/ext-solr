3e6704da418788a254f0575c218769cad78b31bd
Jens Jacobsen <typo3@jens-jacobsen.de>
2020-07-03 09:05:50 +0200

    [FEATURE] Add scheduler task to optimize solr cores of a site
    This task allows you to optimize the indexes of given cores for a site at a planned time.
    
    See: https://docs.typo3.org/p/apache-solr-for-typo3/solr/11.1/en-us/Backend/Scheduler.html#optimizing-cores-of-a-site
    
    Fixes: #2649

---
+++ Classes/Task/OptimizeIndexTask.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace ApacheSolrForTypo3\Solr\Task;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use ApacheSolrForTypo3\Solr\ConnectionManager;
+use ApacheSolrForTypo3\Solr\NoSolrConnectionFoundException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Scheduler task to empty the indexes of a site and re-initialize the
+ * Solr Index Queue thus making the indexer re-index the site.
+ *
+ * @author Jens Jacobsen <typo3@jens-jacobsen.de>
+ */
+class OptimizeIndexTask extends AbstractSolrTask
+{
+    /**
+     * Cores to optimize.
+     *
+     * @var array
+     */
+    protected $coresToOptimizeIndex = [];
+
+    /**
+     * Optimizes all Solr indexes for selected cores and returns TRUE if the execution was successful
+     *
+     * @return bool Returns TRUE on success, FALSE on failure.
+     * @throws NoSolrConnectionFoundException
+     * @noinspection PhpMissingReturnTypeInspection
+     * @noinspection PhpUnused
+     */
+    public function execute()
+    {
+        $optimizeResult = true;
+        $solrServers = GeneralUtility::makeInstance(ConnectionManager::class)->getConnectionsBySite($this->getSite());
+        foreach ($solrServers as $solrServer) {
+            $writeService = $solrServer->getWriteService();
+            $corePath = $writeService->getCorePath();
+            if (!in_array($corePath, $this->coresToOptimizeIndex)) {
+                continue;
+            }
+            $result = $writeService->optimizeIndex();
+            if ($result->getResponse()->getStatusCode() != 200) {
+                $optimizeResult = false;
+            }
+        }
+        return $optimizeResult;
+    }
+
+    /**
+     * Gets the cores to optimize.
+     *
+     * @return array
+     */
+    public function getCoresToOptimizeIndex(): array
+    {
+        return $this->coresToOptimizeIndex;
+    }
+
+    /**
+     * Sets the cores to optimize.
+     *
+     * @param array $coresToOptimizeIndex
+     */
+    public function setCoresToOptimizeIndex(array $coresToOptimizeIndex): void
+    {
+        $this->coresToOptimizeIndex = $coresToOptimizeIndex;
+    }
+
+    /**
+     * This method is designed to return some additional information about the task,
+     * that may help to set it apart from other tasks from the same class
+     * This additional information is used - for example - in the Scheduler's BE module
+     * This method should be implemented in most task classes
+     *
+     * @return string Information to display
+     * @noinspection PhpMissingReturnTypeInspection
+     */
+    public function getAdditionalInformation()
+    {
+        $site = $this->getSite();
+        if (is_null($site)) {
+            return 'Invalid site configuration for scheduler please re-create the task!';
+        }
+
+        $information = 'Site: ' . $this->getSite()->getLabel();
+        if (!empty($this->coresToOptimizeIndex)) {
+            $information .= PHP_EOL . 'Corepaths: ' . implode(', ', $this->coresToOptimizeIndex);
+        }
+        return $information;
+    }
+}
---
+++ Tests/Unit/System/Solr/Service/SolrWriteServiceTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace ApacheSolrForTypo3\Solr\Tests\Unit\System\Solr\Service;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2020 Jens Jacobsen <typo3@jens-jacobsen.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use ApacheSolrForTypo3\Solr\System\Solr\Service\SolrWriteService;
+use ApacheSolrForTypo3\Solr\Tests\Unit\UnitTest;
+use Solarium\Client;
+use Solarium\Core\Client\Response;
+use Solarium\QueryType\Update\Query\Query;
+use Solarium\QueryType\Update\Result;
+
+/**
+ * Tests the ApacheSolrForTypo3\Solr\SolrService class
+ *
+ * @author Jens Jacobsen <typo3@jens-jacobsen.de>
+ */
+class SolrWriteServiceTest extends UnitTest
+{
+    /**
+     * @var Response
+     */
+    protected $responseMock;
+
+    /**
+     * @var Result
+     */
+    protected $resultMock;
+
+    /**
+     * @var Client
+     */
+    protected $clientMock;
+
+    /**
+     * @var SolrWriteService
+     */
+    protected $service;
+
+    public function setUp() {
+        parent::setUp();
+        $this->responseMock = $this->getDumbMock(Response::class);
+
+        $this->resultMock = $this->getDumbMock(Result::class);
+        $this->resultMock->expects($this->any())->method('getResponse')->willReturn($this->responseMock);
+        $this->clientMock = $this->getDumbMock(Client::class);
+
+        $this->service = new SolrWriteService($this->clientMock);
+    }
+
+    /**
+     * @test
+     */
+    public function canRunOptimizeIndex()
+    {
+        $this->responseMock->expects($this->once())->method('getStatusCode')->willReturn(200);
+        $this->clientMock->expects($this->once())->method('createUpdate')->willReturn($this->getDumbMock(Query::class));
+        $this->clientMock->expects($this->once())->method('update')->willReturn($this->resultMock);
+
+        $result = $this->service->optimizeIndex();
+        $this->assertSame(200, $result->getResponse()->getStatusCode(), 'Expecting to get a 200 OK response');
+    }
+}
