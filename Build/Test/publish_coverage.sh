d6b845165a0b4f2542874c5669a6ff250c36b2e2
Rafael Kähm <rafael.kaehm@dkd.de>
2020-08-12 23:08:03 +0200

    [BUGFIX] fix integration tests within 10.x-release-branch

--- Build/Test/publish_coverage.sh
+++ Build/Test/publish_coverage.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export TYPO3_BIN_DIR="$(pwd)/.Build/bin/"
-export COMPOSER_BIN_DIR="$HOME/.composer/vendor/bin"
-
-# Add TYPO3_BIN_DIR and COMPOSER_BIN_DIR to $PATH
-export PATH="$TYPO3_BIN_DIR:$COMPOSER_BIN_DIR:$PATH"
-
-ls -la
-ocular code-coverage:upload --format=php-clover coverage.unit.clover
-ocular code-coverage:upload --format=php-clover coverage.integration.clover
+wget https://github.com/scrutinizer-ci/ocular/releases/download/1.6.0/ocular.phar
+php ocular.phar code-coverage:upload --format=php-clover coverage.unit.clover
+php ocular.phar code-coverage:upload --format=php-clover coverage.integration.clover
+php ocular.phar code-coverage:upload --format=php-clover coverage.integration.frontend.clover
+rm ocular.phar
