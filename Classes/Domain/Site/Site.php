f9b200aab27d7c54f7c83e751b96e16d5f385f7e
Achim Fritz <achim.fritz@b13.de>
2020-11-19 16:52:44 +0100

    [BUGFIX:BACKPORT:10] disabled Solr Sites #2795
    SiteRepository should not return sites with disabled solr
    when calling "getAvailableSites"
    
    Resolves: #2795

--- Classes/Domain/Site/Site.php
+++ Classes/Domain/Site/Site.php
@@ -299,6 +299,11 @@ abstract class Site implements SiteInterface
         return $configs;
     }
 
+    public function isEnabled(): bool
+    {
+        return !empty($this->getAllSolrConnectionConfigurations());
+    }
+
     /**
      * @param int $languageId
      * @return array
