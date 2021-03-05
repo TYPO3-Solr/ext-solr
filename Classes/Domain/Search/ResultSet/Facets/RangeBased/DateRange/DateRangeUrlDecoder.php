5a9e9f83760a38af442c149766c103898751f875
Lars Tode <lars.tode@dkd.de>
2021-03-05 09:32:07 +0100

    [TASK] Code documentation
    Update licence information.
    
    Declare return type for methods where return value is cast to a
    specific data type.
    
    Adjusts code format in order to make it easier to read.
    
    Fixes: #481

--- Classes/Domain/Search/ResultSet/Facets/RangeBased/DateRange/DateRangeUrlDecoder.php
+++ Classes/Domain/Search/ResultSet/Facets/RangeBased/DateRange/DateRangeUrlDecoder.php
@@ -1,29 +1,18 @@
 <?php
 namespace ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Facets\RangeBased\DateRange;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2010-2011 Markus Goldbach <markus.goldbach@dkd.de>
- *  (c) 2012-2015 Ingo Renner <ingo@typo3.org>
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Facets\FacetUrlDecoderInterface;
 use ApacheSolrForTypo3\Solr\System\DateTime\FormatService;
@@ -33,6 +22,8 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * Parser to build solr range queries from tx_solr[filter]
  *
  * @author Markus Goldbach <markus.goldbach@dkd.de>
+ * @copyright (c) 2010-2011 Markus Goldbach <markus.goldbach@dkd.de>
+ * @copyright (c) 2012-2015 Ingo Renner <ingo@typo3.org>
  */
 class DateRangeUrlDecoder implements FacetUrlDecoderInterface
 {
@@ -56,6 +47,7 @@ class DateRangeUrlDecoder implements FacetUrlDecoderInterface
     {
         list($dateRangeStart, $dateRangeEnd) = explode(self::DELIMITER, $dateRange);
 
+        /* @var FormatService $formatService */
         $formatService = GeneralUtility::makeInstance(FormatService::class);
         $fromPart = '*';
         if($dateRangeStart !== ''){
