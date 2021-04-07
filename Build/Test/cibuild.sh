01af1cb3c7e46722df5792f237ed5a04ce0c50fa
Markus Friedrich <markus.friedrich@dkd.de>
2021-04-07 12:04:17 +0200

    [TASK] Add XML linter
    Integrates a XML linter into the cibuild script.
    
    Resolves: #2874

--- Build/Test/cibuild.sh
+++ Build/Test/cibuild.sh
@@ -1,30 +1,51 @@
 #!/usr/bin/env bash
 
-echo "PWD: $(pwd)"
-
 export TYPO3_PATH_WEB="$(pwd)/.Build/Web/"
 export TYPO3_PATH_PACKAGES="$(pwd)/.Build/vendor/"
 
 export TYPO3_BIN_DIR="$(pwd)/.Build/bin/"
-export COMPOSER_BIN_DIR="$HOME/.composer/vendor/bin"
+export COMPOSER_BIN_DIR="$(composer config home)/vendor/bin"
 
 # Add TYPO3_BIN_DIR and COMPOSER_BIN_DIR to $PATH
 export PATH="$TYPO3_BIN_DIR:$COMPOSER_BIN_DIR:$PATH"
+
+echo "PWD: $(pwd)"
+echo "COMPOSER_BIN_DIR: $COMPOSER_BIN_DIR"
+echo "PATH: $PATH"
 
 echo "Run PHP Lint"
 find . -name \*.php ! -path "./.Build/*" | parallel --gnu php -d display_errors=stderr -l {} > /dev/null \;
 
 # use from vendor dir
 php-cs-fixer --version > /dev/null 2>&1
-if [ $? -eq "0" ]; then
+if [ $? -eq "0" ]
+  then
     echo "Check PSR-2 compliance"
     php-cs-fixer fix --diff --verbose --dry-run --rules='{"function_declaration": {"closure_function_spacing": "none"}}' Classes
 
     if [ $? -ne "0" ]; then
-        echo "Some files are not PSR-2 compliant"
-        echo "Please fix the files listed above"
-        exit 1
+      echo "Some files are not PSR-2 compliant"
+      echo "Please fix the files listed above"
+      exit 1
     fi
+  else
+    echo "PHP CS Fixer not found, skipping PHP linting."
+fi
+
+echo "Run XML Lint"
+xmllint --version > /dev/null 2>&1
+if [ $? -eq "0" ]
+  then
+    echo "Check syntax of XML files"
+    xmllint Resources/Private/Language/ -p '*.xlf'
+
+    if [ $? -ne "0" ]; then
+      echo "Some XML files are not valid"
+      echo "Please fix the files listed above"
+      exit 1
+    fi
+  else
+    echo "XML Lint not found, skipping XML linting."
 fi
 
 
@@ -32,8 +53,8 @@ echo "Run unit tests"
 UNIT_BOOTSTRAP=".Build/vendor/nimut/testing-framework/res/Configuration/UnitTestsBootstrap.php"
 .Build/bin/phpunit --colors -c Build/Test/UnitTests.xml --bootstrap=$UNIT_BOOTSTRAP --coverage-clover=coverage.unit.clover
 if [ $? -ne "0" ]; then
-    echo "Error during running the unit tests please check and fix them"
-    exit 1
+  echo "Error during running the unit tests please check and fix them"
+  exit 1
 fi
 
 #
@@ -41,39 +62,39 @@ fi
 # casing of the TYPO3 core.
 #
 if [ -n $TYPO3_DATABASE_NAME ]; then
-	export typo3DatabaseName=$TYPO3_DATABASE_NAME
+  export typo3DatabaseName=$TYPO3_DATABASE_NAME
 else
-	echo "No environment variable TYPO3_DATABASE_NAME set. Please set it to run the integration tests."
-	exit 1
+  echo "No environment variable TYPO3_DATABASE_NAME set. Please set it to run the integration tests."
+  exit 1
 fi
 
 if [ -n $TYPO3_DATABASE_HOST ]; then
-	export typo3DatabaseHost=$TYPO3_DATABASE_HOST
+  export typo3DatabaseHost=$TYPO3_DATABASE_HOST
 else
-	echo "No environment variable TYPO3_DATABASE_HOST set. Please set it to run the integration tests."
-	exit 1
+  echo "No environment variable TYPO3_DATABASE_HOST set. Please set it to run the integration tests."
+  exit 1
 fi
 
 if [ -n $TYPO3_DATABASE_USERNAME ]; then
-	export typo3DatabaseUsername=$TYPO3_DATABASE_USERNAME
+  export typo3DatabaseUsername=$TYPO3_DATABASE_USERNAME
 else
-	echo "No environment variable TYPO3_DATABASE_USERNAME set. Please set it to run the integration tests."
-	exit 1
+  echo "No environment variable TYPO3_DATABASE_USERNAME set. Please set it to run the integration tests."
+  exit 1
 fi
 
 if [ -n $TYPO3_DATABASE_PASSWORD ]; then
-	export typo3DatabasePassword=$TYPO3_DATABASE_PASSWORD
+  export typo3DatabasePassword=$TYPO3_DATABASE_PASSWORD
 else
-	echo "No environment variable TYPO3_DATABASE_PASSWORD set. Please set it to run the integration tests."
-	exit 1
+  echo "No environment variable TYPO3_DATABASE_PASSWORD set. Please set it to run the integration tests."
+  exit 1
 fi
 
 echo "Run integration tests"
 INTEGRATION_BOOTSTRAP=".Build/vendor/nimut/testing-framework/res/Configuration/FunctionalTestsBootstrap.php"
 .Build/bin/phpunit --colors -c Build/Test/IntegrationTests.xml --bootstrap=$INTEGRATION_BOOTSTRAP --coverage-clover=coverage.integration.clover
 if [ $? -ne "0" ]; then
-    echo "Error during running the integration tests please check and fix them"
-    exit 1
+  echo "Error during running the integration tests please check and fix them"
+  exit 1
 fi
 
 echo "Run frontend-related integration tests"
