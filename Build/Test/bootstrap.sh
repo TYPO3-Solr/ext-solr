d978016c0238c20c6fd4278604c612e918d0203f
Markus Friedrich <markus.friedrich@dkd.de>
2021-04-07 12:04:17 +0200

    [TASK] Add XML linter
    Integrates a XML linter into the cibuild script.
    
    Resolves: #2874

--- Build/Test/bootstrap.sh
+++ Build/Test/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ ! -z ${BASH_SOURCE[0]} ]]; then
-    SCRIPTPATH=$( cd $(dirname ${BASH_SOURCE[0]}) ; pwd -P )
+  SCRIPTPATH=$( cd $(dirname ${BASH_SOURCE[0]}) ; pwd -P )
 else
-    SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+  SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 fi
 
 EXTENSION_ROOTPATH="$SCRIPTPATH/../../"
@@ -17,44 +17,44 @@ DEFAULT_TYPO3_DATABASE_USERNAME="root"
 DEFAULT_TYPO3_DATABASE_PASSWORD="supersecret"
 
 if [[ $* == *--use-defaults* ]]; then
-    export TYPO3_VERSION=$DEFAULT_TYPO3_VERSION
-    export PHP_CS_FIXER_VERSION=$DEFAULT_PHP_CS_FIXER_VERSION
-    export TYPO3_DATABASE_HOST=$DEFAULT_TYPO3_DATABASE_HOST
-    export TYPO3_DATABASE_NAME=$DEFAULT_TYPO3_DATABASE_NAME
-    export TYPO3_DATABASE_USERNAME=$DEFAULT_TYPO3_DATABASE_USERNAME
-    export TYPO3_DATABASE_PASSWORD=$DEFAULT_TYPO3_DATABASE_PASSWORD
+  export TYPO3_VERSION=$DEFAULT_TYPO3_VERSION
+  export PHP_CS_FIXER_VERSION=$DEFAULT_PHP_CS_FIXER_VERSION
+  export TYPO3_DATABASE_HOST=$DEFAULT_TYPO3_DATABASE_HOST
+  export TYPO3_DATABASE_NAME=$DEFAULT_TYPO3_DATABASE_NAME
+  export TYPO3_DATABASE_USERNAME=$DEFAULT_TYPO3_DATABASE_USERNAME
+  export TYPO3_DATABASE_PASSWORD=$DEFAULT_TYPO3_DATABASE_PASSWORD
 fi
 
 if [[ $* == *--local* ]]; then
-    echo -n "Choose a TYPO3 Version [dafults: " $DEFAULT_TYPO3_VERSION"] : "
-    read typo3Version
-    if [ -z "$typo3Version" ]; then typo3Version=$DEFAULT_TYPO3_VERSION; fi
-    export TYPO3_VERSION=$typo3Version
+  echo -n "Choose a TYPO3 Version [defaults: " $DEFAULT_TYPO3_VERSION"] : "
+  read typo3Version
+  if [ -z "$typo3Version" ]; then typo3Version=$DEFAULT_TYPO3_VERSION; fi
+  export TYPO3_VERSION=$typo3Version
 
-    echo -n "Choose a php-cs-fixer version [dafults: " $DEFAULT_PHP_CS_FIXER_VERSION"] : "
-    read phpCSFixerVersion
-    if [ -z "$phpCSFixerVersion" ]; then phpCSFixerVersion=$DEFAULT_PHP_CS_FIXER_VERSION; fi
-    export PHP_CS_FIXER_VERSION=$phpCSFixerVersion
+  echo -n "Choose a php-cs-fixer version [defaults: " $DEFAULT_PHP_CS_FIXER_VERSION"] : "
+  read phpCSFixerVersion
+  if [ -z "$phpCSFixerVersion" ]; then phpCSFixerVersion=$DEFAULT_PHP_CS_FIXER_VERSION; fi
+  export PHP_CS_FIXER_VERSION=$phpCSFixerVersion
 
-    echo -n "Choose a database hostname: [dafults: " $DEFAULT_TYPO3_DATABASE_HOST"] : "
-    read typo3DbHost
-    if [ -z "$typo3DbHost" ]; then typo3DbHost=$DEFAULT_TYPO3_DATABASE_HOST; fi
-    export TYPO3_DATABASE_HOST=$typo3DbHost
+  echo -n "Choose a database hostname: [defaults: " $DEFAULT_TYPO3_DATABASE_HOST"] : "
+  read typo3DbHost
+  if [ -z "$typo3DbHost" ]; then typo3DbHost=$DEFAULT_TYPO3_DATABASE_HOST; fi
+  export TYPO3_DATABASE_HOST=$typo3DbHost
 
-    echo -n "Choose a database name: [dafults: " $DEFAULT_TYPO3_DATABASE_NAME"] : "
-    read typo3DbName
-    if [ -z "$typo3DbName" ]; then typo3DbName=$DEFAULT_TYPO3_DATABASE_NAME; fi
-    export TYPO3_DATABASE_NAME=$typo3DbName
+  echo -n "Choose a database name: [defaults: " $DEFAULT_TYPO3_DATABASE_NAME"] : "
+  read typo3DbName
+  if [ -z "$typo3DbName" ]; then typo3DbName=$DEFAULT_TYPO3_DATABASE_NAME; fi
+  export TYPO3_DATABASE_NAME=$typo3DbName
 
-    echo -n "Choose a database user: [dafults: " $DEFAULT_TYPO3_DATABASE_USERNAME"] : "
-    read typo3DbUser
-    if [ -z "$typo3DbUser" ]; then typo3DbUser=$DEFAULT_TYPO3_DATABASE_USERNAME; fi
-    export TYPO3_DATABASE_USERNAME=$typo3DbUser
+  echo -n "Choose a database user: [defaults: " $DEFAULT_TYPO3_DATABASE_USERNAME"] : "
+  read typo3DbUser
+  if [ -z "$typo3DbUser" ]; then typo3DbUser=$DEFAULT_TYPO3_DATABASE_USERNAME; fi
+  export TYPO3_DATABASE_USERNAME=$typo3DbUser
 
-    echo -n "Choose a database password: [dafults: " $DEFAULT_TYPO3_DATABASE_PASSWORD"] : "
-    read typo3DbPassword
-    if [ -z "$typo3DbPassword" ]; then typo3DbPassword=$DEFAULT_TYPO3_DATABASE_PASSWORD; fi
-    export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
+  echo -n "Choose a database password: [defaults: " $DEFAULT_TYPO3_DATABASE_PASSWORD"] : "
+  read typo3DbPassword
+  if [ -z "$typo3DbPassword" ]; then typo3DbPassword=$DEFAULT_TYPO3_DATABASE_PASSWORD; fi
+  export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
 fi
 
 echo "Using TYPO3 Version: $TYPO3_VERSION"
@@ -65,26 +65,26 @@ echo "Using database user: $TYPO3_DATABASE_USERNAME"
 echo "Using database password: $TYPO3_DATABASE_PASSWORD"
 
 if [ -z $TYPO3_VERSION ]; then
-	echo "Must set env var TYPO3_VERSION (e.g. dev-master or ^10.4)"
-	exit 1
+  echo "Must set env var TYPO3_VERSION (e.g. dev-master or ^10.4)"
+  exit 1
 fi
 
 if ! wget --version > /dev/null 2>&1
 then
-	echo "Couldn't find wget."
-	exit 1
+  echo "Couldn't find wget."
+  exit 1
 fi
 
 # Install build tools
 echo "Install build tools: "
 if ! composer global require \
   friendsofphp/php-cs-fixer:"$PHP_CS_FIXER_VERSION" \
-  namelesscoder/typo3-repository-client
+  namelesscoder/typo3-repository-client \
+  sclable/xml-lint
 then
-	echo "The build tools(php-cs-fixer, typo3-repository-client) could not be installed. Please fix this issue."
-	exit 1
+  echo "The build tools(php-cs-fixer, typo3-repository-client) could not be installed. Please fix this issue."
+  exit 1
 fi
-
 
 # Setup TYPO3 environment variables
 export TYPO3_PATH_PACKAGES="${EXTENSION_ROOTPATH}.Build/vendor/"
@@ -95,8 +95,8 @@ echo "Using web path $TYPO3_PATH_WEB"
 
 # Install TYPO3 sources
 if [[ $TYPO3_VERSION = *"master"* ]]; then
-    composer config minimum-stability dev
-    TYPO3_MASTER_DEPENDENCIES='nimut/testing-framework:dev-master'
+  composer config minimum-stability dev
+  TYPO3_MASTER_DEPENDENCIES='nimut/testing-framework:dev-master'
 fi
 
 # Temporary downgrades
@@ -119,19 +119,19 @@ if ! composer require --dev --update-with-dependencies --prefer-source \
   typo3/cms-tstemplate:"$TYPO3_VERSION" \
   typo3/cms-install:"$TYPO3_VERSION" $TYPO3_TEMPORARY_DOWNGRADES $TYPO3_MASTER_DEPENDENCIES
 then
-	echo "The test environment could not be installed by composer as expected. Please fix this issue."
-	exit 1
+  echo "The test environment could not be installed by composer as expected. Please fix this issue."
+  exit 1
 fi
 
 mkdir -p $TYPO3_PATH_WEB/uploads $TYPO3_PATH_WEB/typo3temp
 
 if [[ $* != *--skip-solr-install* ]]; then
-    # Setup Solr Using our install script
-    echo "Setup Solr Using our install script: "
-    chmod 500 ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh
-    if ! ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
-    then
-      echo "Apache Solr server could not be installed or started. Please fix this issue."
-      exit 1
-    fi
+  # Setup Solr Using our install script
+  echo "Setup Solr Using our install script: "
+  chmod 500 ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh
+  if ! ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
+  then
+    echo "Apache Solr server could not be installed or started. Please fix this issue."
+    exit 1
+  fi
 fi
