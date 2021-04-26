61fc8d19b6fbc28c104c1b9d799c2aecc1805852
Rafael Kähm <rafael.kaehm@dkd.de>
2021-04-26 08:29:14 +0200

    [BUGFIX] remove escaping on suggestion prefix
    Since solarium escapes the special uri characters with RFC1738,
    there are no needs to do the same within the EXT:Solr.
    
    see:
    https://github.com/solariumphp/solarium/blob/97a9df0c2d9ba35db3c747b30c0809702296f18b/src/Component/RequestBuilder/RequestParamsTrait.php#L153
    
    This is the first step of removing the escaping/encoding from EXT:Solr API.
    Other components of EXT:solr will be fixed as well in different pull requests.

--- Classes/Domain/Search/Query/SuggestQuery.php
+++ Classes/Domain/Search/Query/SuggestQuery.php
@@ -1,31 +1,20 @@
 <?php
 namespace ApacheSolrForTypo3\Solr\Domain\Search\Query;
 
-/***************************************************************
- *  Copyright notice
+/*
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2009-2015 Ingo Renner <ingo@typo3.org>
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 use ApacheSolrForTypo3\Solr\Domain\Search\Query\ParameterBuilder\ReturnFields;
-use ApacheSolrForTypo3\Solr\Domain\Search\Query\Helper\EscapeService;
 use ApacheSolrForTypo3\Solr\System\Configuration\TypoScriptConfiguration;
 use ApacheSolrForTypo3\Solr\Util;
 
@@ -33,6 +22,7 @@ use ApacheSolrForTypo3\Solr\Util;
  * A query specialized to get search suggestions
  *
  * @author Ingo Renner <ingo@typo3.org>
+ * @copyright (c) 2009-2015 Ingo Renner <ingo@typo3.org>
  */
 class SuggestQuery extends Query
 {
@@ -63,7 +53,7 @@ class SuggestQuery extends Query
         $this->configuration = $solrConfiguration->getObjectByPathOrDefault('plugin.tx_solr.suggest.', []);
 
         if (!empty($this->configuration['treatMultipleTermsAsSingleTerm'])) {
-            $this->prefix = EscapeService::escape($keywords);
+            $this->prefix = $keywords;
         } else {
             $matches = [];
             preg_match('/^(:?(.* |))([^ ]+)$/', $keywords, $matches);
@@ -71,7 +61,7 @@ class SuggestQuery extends Query
             $partialKeyword = trim($matches[3]);
 
             $this->setQuery($fullKeywords);
-            $this->prefix = EscapeService::escape($partialKeyword);
+            $this->prefix = $partialKeyword;
         }
 
         $this->getEDisMax()->setQueryAlternative('*:*');
