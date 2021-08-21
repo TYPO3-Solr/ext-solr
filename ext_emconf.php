b6cc8b003c5524752cd6e79f76baa84dc0a821b7
Andreas Beutel <andreas.beutel@mehrwert.de>
2021-08-21 16:35:08 +0200

    [BUGFIX] Set proper version constraints for TYPO3 10.4
    Fixes:  #3004

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Apache Solr for TYPO3 - Enterprise Search',
     'description' => 'Apache Solr for TYPO3 is the enterprise search server you were looking for with special features such as Faceted Search or Synonym Support and incredibly fast response times of results within milliseconds.',
-    'version' => '11.1.0',
+    'version' => '11.1.1',
     'state' => 'stable',
     'category' => 'plugin',
     'author' => 'Ingo Renner, Timo Hund, Markus Friedrich',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'scheduler' => '',
-            'typo3' => '10.5.10-10.5.99'
+            'typo3' => '10.4.10-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [
