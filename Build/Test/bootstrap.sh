88d5f50a6bf35456bd58abb5567179196f795e6d
Rafael Kähm <rafael.kaehm@dkd.de>
2021-10-24 13:05:27 +0200

    [TASK] Setup Github Actions :: Basics @release-11.0.x
    This change contains:
    * Basic integration of GitHub Actions
      * Caching of:
        * docker image
        * using Docker image instead of Apache solr installation script
        * composer installation state caching aware of:
           * TYPO3 git commit hash of required version
           * solarium/solarium versions string
           * PHP version
       Results to 11 seconds on caching vs 4:30 minutes on composer install.
    * Use single workflow file within multiple branches, but different build matrix settings for them.
    * Integration of Tailor to publish releases in TER
       * Run "Publish new version to TER" on PHP version declared in
         `Resources/Private/Php/ComposerLibraries/composer.json`
          '.config.platform.php'`
    * Allow to fail the jobs from matrix within TYPO3 `-dev` versions.
    * Uploads the coverage to Scrutinizer
    
    Relates: #2977, #2999, #3072

--- Build/Test/bootstrap.sh
+++ Build/Test/bootstrap.sh
@@ -9,39 +9,46 @@ fi
 EXTENSION_ROOTPATH="$SCRIPTPATH/../../"
 SOLR_INSTALL_PATH="/opt/solr-tomcat/"
 
+DEFAULT_TYPO3_VERSION="10"
+DEFAULT_PHP_CS_FIXER_VERSION="^2.16.1"
+DEFAULT_TYPO3_DATABASE_HOST="localhost"
+DEFAULT_TYPO3_DATABASE_NAME="test"
+DEFAULT_TYPO3_DATABASE_USERNAME="root"
+DEFAULT_TYPO3_DATABASE_PASSWORD="supersecret"
+
 if [[ $* == *--use-defaults* ]]; then
-    export TYPO3_VERSION="^10.4"
-    export PHP_CS_FIXER_VERSION="^2.16.1"
-    export TYPO3_DATABASE_HOST="localhost"
-    export TYPO3_DATABASE_NAME="test"
-    export TYPO3_DATABASE_USERNAME="root"
-    export TYPO3_DATABASE_PASSWORD="supersecret"
+  export TYPO3_VERSION=$DEFAULT_TYPO3_VERSION
+  export PHP_CS_FIXER_VERSION=$DEFAULT_PHP_CS_FIXER_VERSION
+  export TYPO3_DATABASE_HOST=$DEFAULT_TYPO3_DATABASE_HOST
+  export TYPO3_DATABASE_NAME=$DEFAULT_TYPO3_DATABASE_NAME
+  export TYPO3_DATABASE_USERNAME=$DEFAULT_TYPO3_DATABASE_USERNAME
+  export TYPO3_DATABASE_PASSWORD=$DEFAULT_TYPO3_DATABASE_PASSWORD
 fi
 
 if [[ $* == *--local* ]]; then
-    echo -n "Choose a TYPO3 Version (e.g. dev-master,^10.4): "
-    read typo3Version
-    export TYPO3_VERSION=$typo3Version
+  echo -n "Choose a TYPO3 Version (e.g. dev-master,^10.4): "
+  read typo3Version
+  export TYPO3_VERSION=$typo3Version
 
-    echo -n "Choose a php-cs-fixer version (v2.16.1): "
-    read phpCSFixerVersion
-    export PHP_CS_FIXER_VERSION=$phpCSFixerVersion
+  echo -n "Choose a php-cs-fixer version (v2.16.1): "
+  read phpCSFixerVersion
+  export PHP_CS_FIXER_VERSION=$phpCSFixerVersion
 
-    echo -n "Choose a database hostname: "
-    read typo3DbHost
-    export TYPO3_DATABASE_HOST=$typo3DbHost
+  echo -n "Choose a database hostname: "
+  read typo3DbHost
+  export TYPO3_DATABASE_HOST=$typo3DbHost
 
-    echo -n "Choose a database name: "
-    read typo3DbName
-    export TYPO3_DATABASE_NAME=$typo3DbName
+  echo -n "Choose a database name: "
+  read typo3DbName
+  export TYPO3_DATABASE_NAME=$typo3DbName
 
-    echo -n "Choose a database user: "
-    read typo3DbUser
-    export TYPO3_DATABASE_USERNAME=$typo3DbUser
+  echo -n "Choose a database user: "
+  read typo3DbUser
+  export TYPO3_DATABASE_USERNAME=$typo3DbUser
 
-    echo -n "Choose a database password: "
-    read typo3DbPassword
-    export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
+  echo -n "Choose a database password: "
+  read typo3DbPassword
+  export TYPO3_DATABASE_PASSWORD=$typo3DbPassword
 fi
 
 echo "Using TYPO3 Version: $TYPO3_VERSION"
@@ -55,10 +62,15 @@ if [ -z $TYPO3_VERSION ]; then
 	exit 1
 fi
 
-wget --version > /dev/null 2>&1
-if [ $? -ne "0" ]; then
-	echo "Couldn't find wget."
-	exit 1
+if ! wget --version > /dev/null 2>&1
+then
+  echo "Couldn't find wget."
+  exit 1
+fi
+
+# Use latest TYPO3 LTS stable version, if version number is compatible with get.typo3.org API
+if [[ $TYPO3_VERSION =~ ^[0-9]+$ ]] ; then
+  TYPO3_VERSION=$("${BASH_SOURCE%/*}/../Helpers/TYPO3_GET_LATEST_VERSION.sh" "$TYPO3_VERSION")
 fi
 
 # Install build tools
@@ -76,19 +88,19 @@ echo "Using web path $TYPO3_PATH_WEB"
 # Install TYPO3 sources
 
 if [[ $TYPO3_VERSION = *"dev"* ]]; then
-    composer config minimum-stability dev
+  composer config minimum-stability dev
 fi
 
 if [[ $TYPO3_VERSION = *"master"* ]]; then
-    TYPO3_MASTER_DEPENDENCIES='nimut/testing-framework:dev-master'
+  TYPO3_MASTER_DEPENDENCIES='nimut/testing-framework:dev-master'
 fi
 
 # Temporary downgrades
 CURRENT_PHP_VERSION=$(php -r "echo PHP_VERSION;" | grep --only-matching --perl-regexp "7.\d+")
 if [[ ! $CURRENT_PHP_VERSION = *"7.2"* ]] && [[ $TYPO3_VERSION = *"10.4"* ]]; then
-    TYPO3_TEMPORARY_DOWNGRADES="doctrine/dbal:2.11.3"
+  TYPO3_TEMPORARY_DOWNGRADES="doctrine/dbal:2.11.3"
 elif [[ $CURRENT_PHP_VERSION = *"7.2"* ]] && [[ $TYPO3_VERSION = *"10.4"* ]]; then
-    TYPO3_TEMPORARY_DOWNGRADES="doctrine/dbal:2.10.4"
+  TYPO3_TEMPORARY_DOWNGRADES="doctrine/dbal:2.10.4"
 fi
 
 if ! composer require --dev --update-with-dependencies --prefer-source \
@@ -111,8 +123,8 @@ mkdir -p $TYPO3_PATH_WEB/uploads $TYPO3_PATH_WEB/typo3temp
 
 
 if [[ $* != *--skip-solr-install* ]]; then
-    # Setup Solr Using our install script
-    echo "Setup Solr Using our install script: "
-    chmod 500 ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh
-    ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
+  # Setup Solr Using our install script
+  echo "Setup Solr Using our install script: "
+  chmod 500 ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh
+  ${EXTENSION_ROOTPATH}Resources/Private/Install/install-solr.sh -d "$HOME/solr" -t
 fi
