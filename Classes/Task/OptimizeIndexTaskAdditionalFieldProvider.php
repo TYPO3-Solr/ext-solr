685b767d4db1159391a6061adc167c6caf18df1c
Christopher Schnell <christopher.schnell@swisstph.ch>
2022-06-20 15:16:25 +0200

    [BUGFIX] AbstractSolrTask::setRootPageId(): Argument #1 () must be of type int, string given
    Fixes: #3267

--- Classes/Task/OptimizeIndexTaskAdditionalFieldProvider.php
+++ Classes/Task/OptimizeIndexTaskAdditionalFieldProvider.php
@@ -245,7 +245,7 @@ class OptimizeIndexTaskAdditionalFieldProvider extends AbstractAdditionalFieldPr
         }
 
         $task->/** @scrutinizer ignore-call */
-            setRootPageId($submittedData['site']);
+            setRootPageId((int)$submittedData['site']);
 
         $cores = [];
         if (!empty($submittedData['cores'])) {
