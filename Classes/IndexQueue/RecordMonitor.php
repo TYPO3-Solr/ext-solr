3fa4ff49662a23fc75b02667823e9f1bded0f413
Rafael Kähm <rafael.kaehm@dkd.de>
2022-07-18 09:35:28 +0200

    [BUGFIX] Ensure array value is set when accessing
    Fixes: #3269

--- Classes/IndexQueue/RecordMonitor.php
+++ Classes/IndexQueue/RecordMonitor.php
@@ -130,7 +130,11 @@ class RecordMonitor
         }
 
         if ($status === 'new' && !MathUtility::canBeInterpretedAsInteger($recordUid)) {
-            $recordUid = $tceMain->substNEWwithIDs[$recordUid];
+            if (isset($tceMain->substNEWwithIDs[$recordUid])) {
+                $recordUid = $tceMain->substNEWwithIDs[$recordUid];
+            } else {
+                return;
+            }
         }
         if (Util::isDraftRecord($table, (int)$recordUid)) {
             // skip workspaces: index only LIVE workspace
