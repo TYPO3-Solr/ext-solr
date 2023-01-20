bd8818a1344aa4e4f234783a0b7f150ea496d784
Rafael Kähm <rafael.kaehm@dkd.de>
2023-01-21 00:48:12 +0100

    [TASK:T12] Refactor basic files and method argument hinting

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,32 @@
 <?php
 
-defined('TYPO3') || die();
+use ApacheSolrForTypo3\Solr\Controller\Backend\Search\IndexAdministrationModuleController;
+use ApacheSolrForTypo3\Solr\System\Configuration\ExtensionConfiguration;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Http\ApplicationType;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die('Access denied.');
 
 (function () {
-    if (TYPO3_MODE == 'BE') {
+    if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
+        && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
+    ) {
         $modulePrefix = 'extensions-solr-module';
-        $svgProvider = \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class;
-        /* @var \ApacheSolrForTypo3\Solr\System\Configuration\ExtensionConfiguration $extensionConfiguration */
-        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-            \ApacheSolrForTypo3\Solr\System\Configuration\ExtensionConfiguration::class
+        $svgProvider = SvgIconProvider::class;
+        /* @var ExtensionConfiguration $extensionConfiguration */
+        $extensionConfiguration = GeneralUtility::makeInstance(
+            ExtensionConfiguration::class
         );
 
         // register all module icons with extensions-solr-module-modulename
         $extIconPath = 'EXT:solr/Resources/Public/Images/Icons/';
-        /* @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+        /* @var IconRegistry $iconRegistry */
+        $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
         $iconRegistry->registerIcon(
             $modulePrefix . '-main',
             $svgProvider,
@@ -51,7 +63,7 @@ defined('TYPO3') || die();
 
         // Add Main module "APACHE SOLR".
         // Acces to a main module is implicit, as soon as a user has access to at least one of its submodules. To make it possible, main module must be registered in that way and without any Actions!
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+        ExtensionManagementUtility::addModule(
             'searchbackend',
             '',
             '',
@@ -65,7 +77,7 @@ defined('TYPO3') || die();
 
         $treeComponentId = 'TYPO3/CMS/Backend/PageTree/PageTreeElement';
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        ExtensionUtility::registerModule(
             'Solr',
             'searchbackend',
             'Info',
@@ -81,7 +93,7 @@ defined('TYPO3') || die();
             ]
         );
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        ExtensionUtility::registerModule(
             'Solr',
             'searchbackend',
             'CoreOptimization',
@@ -97,7 +109,7 @@ defined('TYPO3') || die();
             ]
         );
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        ExtensionUtility::registerModule(
             'Solr',
             'searchbackend',
             'IndexQueue',
@@ -114,13 +126,13 @@ defined('TYPO3') || die();
             ]
         );
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        ExtensionUtility::registerModule(
             'Solr',
             'searchbackend',
             'IndexAdministration',
             '',
             [
-                \ApacheSolrForTypo3\Solr\Controller\Backend\Search\IndexAdministrationModuleController::class =>
+                IndexAdministrationModuleController::class =>
                     'index, emptyIndex, clearIndexQueue, reloadIndexConfiguration, switchSite',
             ],
             [
@@ -132,13 +144,22 @@ defined('TYPO3') || die();
         );
 
         // Register Context Sensitive Help (CSH) translation labels
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+        ExtensionManagementUtility::addLLrefForTCAdescr(
             'pages',
             'EXT:solr/Resources/Private/Language/locallang_csh_pages.xlf'
         );
     }
 
-    if ((TYPO3_MODE === 'BE') || (TYPO3_MODE === 'FE' && isset($_POST['TSFE_EDIT']))) {
+    if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
+        && (
+            ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
+            || (
+                ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isFrontend()
+                &&
+                isset($_POST['TSFE_EDIT'])
+            )
+        )
+    ) {
         // the order of registering the garbage collector and the record monitor is important!
         // for certain scenarios items must be removed by GC first, and then be re-added to to Index Queue
 
@@ -157,6 +178,6 @@ defined('TYPO3') || die();
 $isComposerMode = defined('TYPO3_COMPOSER_MODE') && TYPO3_COMPOSER_MODE;
 if (!$isComposerMode) {
     // we load the autoloader for our libraries
-    $dir = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('solr');
+    $dir = ExtensionManagementUtility::extPath('solr');
     require $dir . '/Resources/Private/Php/ComposerLibraries/vendor/autoload.php';
 }
