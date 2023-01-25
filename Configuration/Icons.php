af00b72542035d5d826f4e1b0601637f81904c7a
Lars Tode <lars.tode@dkd.de>
2023-01-25 13:16:11 +0100

    !!![TASK] Icon registration
    Move the icon registration into Configuration/Icons.php
    
    Resolves: #3376

---
+++ Configuration/Icons.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'extensions-solr-module-main' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ModuleSolrMain.svg',
+    ],
+    'extensions-solr-module-solr-core-optimization' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ModuleCoreOptimization.svg',
+    ],
+    'extensions-solr-module-index-administration' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ModuleIndexAdministration.svg',
+    ],
+    'extensions-solr-module-initsolrconnections' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/InitSolrConnections.svg',
+    ],
+    'extensions-solr-module-initsolrconnection' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/InitSolrConnection.svg',
+    ],
+    'extensions-solr-module-index-queue' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ModuleIndexQueue.svg',
+    ],
+    'extensions-solr-module-info' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ModuleInfo.svg',
+    ],
+    'extensions-solr-plugin-contentelement' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:solr/Resources/Public/Images/Icons/ContentElement.svg',
+    ],
+];
