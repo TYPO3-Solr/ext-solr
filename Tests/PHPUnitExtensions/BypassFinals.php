34a95e77f27e6a520af84f88f4714ba327e99a2a
Rafael Kähm <rafael.kaehm@dkd.de>
2023-02-17 13:34:45 +0100

    [TASK] setup dg/bypass-finals PHPUnit extension to be able to mock finals
    Relates: #3376

---
+++ Tests/PHPUnitExtensions/BypassFinals.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace ApacheSolrForTypo3\Solr\Tests\PHPUnitExtensions;
+
+use DG\BypassFinals as DGBypassFinals;
+use PHPUnit\Runner\BeforeTestHook;
+
+final class BypassFinals implements BeforeTestHook
+{
+    public function executeBeforeTest(string $test): void
+    {
+        DGBypassFinals::enable();
+    }
+}
