c3973006e9bd85e04dd4f0d3ac448d2896e71942
Rafael Kähm <rafael.kaehm@dkd.de>
2023-02-17 20:33:42 +0100

    [TASK:T12] Migrate PageModuleSummary to PageContentPreviewRendering
    For more details please refer to: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-98375-RemovedHooksInPageModule.html
    
    Relates:#3376

--- Classes/Backend/SettingsPreviewOnPlugins.php
+++ Classes/Backend/SettingsPreviewOnPlugins.php
@@ -13,56 +13,72 @@
  * The TYPO3 project - inspiring people to share!
  */
 
-namespace ApacheSolrForTypo3\Solr\Controller\Backend;
+namespace ApacheSolrForTypo3\Solr\Backend;
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
-use TYPO3\CMS\Backend\View\PageLayoutView;
+use TYPO3\CMS\Backend\View\Event\PageContentPreviewRenderingEvent;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
+use function str_starts_with;
+
 /**
- * Summary to display flexform settings in the page layout backend module.
- *
- * @author Ingo Renner <ingo@typo3.org>
- * @author Timo Hund <timo.hund@dkd.de>
+ * Summary to display flexform settings of EXT:solr plugin in BE page module.
  */
-class PageModuleSummary
+class SettingsPreviewOnPlugins
 {
-    /**
-     * PageLayoutView
-     *
-     * @var PageLayoutView
-     */
-    protected $pageLayoutView;
+    protected array $pluginsTtContentRecord;
+    private array $flexformData;
+
+    protected array $settings = [];
+
+    public function __construct(
+        protected FlexFormService $flexFormService
+    ) {
+    }
+
+    public function __invoke(PageContentPreviewRenderingEvent $event): void
+    {
+        $this->pluginsTtContentRecord = $event->getRecord();
+        if (
+            $event->getTable() !== 'tt_content'
+            || !str_starts_with($this->pluginsTtContentRecord['list_type'], 'solr_pi_')
+        ) {
+            return;
+        }
+        $this->flexformData = $this->flexFormService->convertFlexFormContentToArray($this->pluginsTtContentRecord['pi_flexform'] ?? '');
+        $event->setPreviewContent($this->getPreviewContent());
+    }
 
     /**
-     * @var array
+     * @return string
      */
-    protected $pluginContentElement = [];
+    protected function getPreviewContent(): string
+    {
+        $this->collectSummary();
 
-    /**
-     * @var array
-     */
-    protected $flexformData = [];
+        /* @var StandaloneView $standaloneView */
+        $standaloneView = GeneralUtility::makeInstance(StandaloneView::class);
+        $standaloneView->setTemplatePathAndFilename(
+            GeneralUtility::getFileAbsFileName('EXT:solr/Resources/Private/Templates/Backend/PageModule/Summary.html')
+        );
 
-    /**
-     * @var array
-     */
-    protected $settings = [];
+        $standaloneView->assignMultiple([
+            'pluginLabel' => $this->getPluginLabel(),
+            'hidden' => $this->pluginsTtContentRecord['hidden'] ?? 0,
+            'settings' => $this->settings,
+        ]);
+        return $standaloneView->render();
+    }
 
     /**
      * Returns information about a plugin's flexform configuration
-     *
-     * @param array $parameters Parameters to the hook
-     * @return string Plugin configuration information
      */
-    public function getSummary(array $parameters)
+    public function collectSummary(): void
     {
-        $this->initialize($parameters['row'], $parameters['pObj']);
-
         $this->addTargetPage();
         $this->addSettingFromFlexForm('Filter', 'search.query.filter');
         $this->addSettingFromFlexForm('Sorting', 'search.query.sortBy');
@@ -71,31 +87,17 @@ class PageModuleSummary
         $this->addSettingFromFlexForm('Boost Query', 'search.query.boostQuery');
         $this->addSettingFromFlexForm('Tie Breaker', 'search.query.tieParameter');
         $this->addSettingFromFlexForm('Template', 'view.templateFiles.results');
-        return $this->render();
-    }
-
-    /**
-     * @param array $contentElement
-     * @param PageLayoutView $pObj
-     */
-    protected function initialize(array $contentElement, PageLayoutView $pObj)
-    {
-        $this->pageLayoutView = $pObj;
-
-        /** @var $service \TYPO3\CMS\Core\Service\FlexFormService::class */
-        $service = GeneralUtility::makeInstance(FlexFormService::class);
-        $this->flexformData = $service->convertFlexFormContentToArray($contentElement['pi_flexform']);
-        $this->pluginContentElement = $contentElement;
     }
 
     /**
      * Adds the target page to the settings.
      */
-    protected function addTargetPage()
+    protected function addTargetPage(): void
     {
         $targetPageId = $this->getFieldFromFlexform('search.targetPage');
         if (!empty($targetPageId)) {
-            $page = BackendUtility::getRecord('pages', $targetPageId, 'title');
+            $page = BackendUtility::getRecord('pages', $targetPageId, 'title')
+                ?? ['title' => 'ERROR: page is gone'];
             $this->settings['Target Page'] = '[' . (int)$targetPageId . '] ' . $page['title'];
         }
     }
@@ -104,12 +106,13 @@ class PageModuleSummary
      * @param string $settingName
      * @param string $flexFormField
      */
-    protected function addSettingFromFlexForm($settingName, $flexFormField)
+    protected function addSettingFromFlexForm(string $settingName, string $flexFormField): void
     {
         $value = $this->getFieldFromFlexform($flexFormField);
 
         if (is_array($value)) {
-            $value = $this->addSettingFromFlexFormArray($settingName, $value);
+            $this->addSettingFromFlexFormArray($settingName, $value);
+            return;
         }
         $this->addSettingIfNotEmpty($settingName, (string)$value);
     }
@@ -117,9 +120,8 @@ class PageModuleSummary
     /**
      * @param string $settingName
      * @param array $values
-     * @return bool
      */
-    protected function addSettingFromFlexFormArray($settingName, $values)
+    protected function addSettingFromFlexFormArray(string $settingName, array $values): void
     {
         foreach ($values as $item) {
             if (!isset($item['field'])) {
@@ -128,8 +130,8 @@ class PageModuleSummary
             $field = $item['field'];
 
             $label = $settingName . ' ';
-            $label .= isset($field['field']) ? $field['field'] : '';
-            $fieldValue = isset($field['value']) ? $field['value'] : '';
+            $label .= $field['field'] ?? '';
+            $fieldValue = $field['value'] ?? '';
             $this->addSettingIfNotEmpty($label, (string)$fieldValue);
         }
     }
@@ -138,7 +140,7 @@ class PageModuleSummary
      * @param string $settingName
      * @param string $value
      */
-    protected function addSettingIfNotEmpty($settingName, $value)
+    protected function addSettingIfNotEmpty(string $settingName, string $value): void
     {
         if (!empty($value)) {
             $this->settings[$settingName] = $value;
@@ -152,28 +154,9 @@ class PageModuleSummary
      * @param string $path name of the field
      * @return mixed|null if nothing found, value if found
      */
-    protected function getFieldFromFlexform(string $path)
+    protected function getFieldFromFlexform(string $path): mixed
     {
         return ObjectAccess::getPropertyPath($this->flexformData, $path);
-    }
-
-    /**
-     * @return string
-     */
-    protected function render()
-    {
-        /** @var $standaloneView StandaloneView */
-        $standaloneView = GeneralUtility::makeInstance(StandaloneView::class);
-        $standaloneView->setTemplatePathAndFilename(
-            GeneralUtility::getFileAbsFileName('EXT:solr/Resources/Private/Templates/Backend/PageModule/Summary.html')
-        );
-
-        $standaloneView->assignMultiple([
-            'pluginLabel' => $this->getPluginLabel(),
-            'hidden' => $this->pluginContentElement['hidden'],
-            'settings' => $this->settings,
-        ]);
-        return $standaloneView->render();
     }
 
     /**
@@ -181,16 +164,16 @@ class PageModuleSummary
      *
      * @return string
      */
-    protected function getPluginLabel()
+    protected function getPluginLabel(): string
     {
-        $label = BackendUtility::getLabelFromItemListMerged($this->pluginContentElement['pid'], 'tt_content', 'list_type', $this->pluginContentElement['list_type']);
+        $label = BackendUtility::getLabelFromItemListMerged($this->pluginsTtContentRecord['pid'], 'tt_content', 'list_type', $this->pluginsTtContentRecord['list_type']);
         if (!empty($label)) {
             $label = $this->getLanguageService()->sL($label);
         } else {
-            $label = sprintf($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.noMatchingValue'), $this->pluginContentElement['list_type']);
+            $label = sprintf($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.noMatchingValue'), $this->pluginsTtContentRecord['list_type']);
         }
 
-        return $this->pageLayoutView->linkEditContent(htmlspecialchars($label), $this->pluginContentElement);
+        return $label;
     }
 
     /**
