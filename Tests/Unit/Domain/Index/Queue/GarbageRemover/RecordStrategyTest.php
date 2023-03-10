67425b0cca9940746dc16a8bb8af86bd99519364
Markus Friedrich <markus.friedrich@dkd.de>
2023-03-10 13:40:37 +0100

    [TASK:BP:11.5] Handle Solr connection exception
    Solr requests may fail due to server errors or faulty queries,
    thrown exceptions or just an invald status code indicate such
    errors.
    
    This commit ensures that exceptions are handled and a valid
    response with a meaningful status code is returned. Handling
    is centralised in SolrService so that other components just
    have to deal with the status code.
    
    Additionally the GarbageCollection is considering this status
    and is logging missing index update.
    
    Resolves: #2903

---
+++ Tests/Unit/Domain/Index/Queue/GarbageRemover/RecordStrategyTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace ApacheSolrForTypo3\Solr\Tests\Unit\Domain\Index\Queue\GarbageRemover;
+
+use ApacheSolrForTypo3\Solr\Domain\Index\Queue\GarbageRemover\RecordStrategy;
+
+/**
+ * RecordStrategy tests
+ */
+class RecordStrategyTest extends AbstractStrategyTest
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->subject = $this->getAccessibleMock(
+            RecordStrategy::class,
+            null,
+            [],
+            '',
+            false
+        );
+    }
+}
