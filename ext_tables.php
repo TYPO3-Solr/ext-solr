4e599501f48ea75e6c0fdc472466764653faeda2
Stefan Froemken <sfroemken@jweiland.net>
2023-06-02 09:46:24 +0200

    Remove CSH registration from ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -6,20 +6,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die('Access denied.');
 
-(static function () {
-    if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
-        && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
-    ) {
-        // Register Context Sensitive Help (CSH) translation labels
-        ExtensionManagementUtility::addLLrefForTCAdescr(
-            'pages',
-            'EXT:solr/Resources/Private/Language/locallang_csh_pages.xlf'
-        );
-    }
-})();
-
-// ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- # ----- #
-
 $isComposerMode = defined('TYPO3_COMPOSER_MODE') && TYPO3_COMPOSER_MODE;
 if (!$isComposerMode) {
     // we load the autoloader for our libraries
