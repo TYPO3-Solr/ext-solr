e3857b6cf47e43cec80432a83e101b0cac909ca1
Sascha Nowak <sascha.nowak@netlogix.de>
2023-07-11 13:09:06 +0200

    [BUGFIX] Handle float values in options facet parser
    Ports: #3722 by @saschanowak

--- Classes/Domain/Search/ResultSet/Facets/OptionBased/Options/OptionsFacetParser.php
+++ Classes/Domain/Search/ResultSet/Facets/OptionBased/Options/OptionsFacetParser.php
@@ -114,7 +114,7 @@ class OptionsFacetParser extends AbstractFacetParser
         foreach ($response->facets->{$facetName}->buckets as $bucket) {
             $optionValue = $bucket->val;
             $optionCount = $bucket->count;
-            $optionsFromSolrResponse[$optionValue] = $optionCount;
+            $optionsFromSolrResponse[(string)$optionValue] = $optionCount;
         }
 
         return $optionsFromSolrResponse;
