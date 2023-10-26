a48f30edb46192915f8a19634fb714f5989cfb18
Rafael Kähm <rafael.kaehm@dkd.de>
2023-10-26 19:11:32 +0200

    [TASK] sync the CI stuff from main branch into 11.6.x
    Relates: #3781

--- Build/Test/cibuild.sh
+++ Build/Test/cibuild.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-TYPO3_PATH_WEB="$(pwd)/.Build/Web/"
-export TYPO3_PATH_WEB
-TYPO3_PATH_PACKAGES="$(pwd)/.Build/vendor/"
-export TYPO3_PATH_PACKAGES
-
-TYPO3_BIN_DIR="$(pwd)/.Build/bin/"
-export TYPO3_BIN_DIR
-export PATH="$TYPO3_BIN_DIR:$PATH"
+EXIT_CODE=0
 
 COMPOSERS_BIN_DIR="$(composer config home)/vendor/bin"
 # Add COMPOSERS_BIN_DIR to $PATH, if not present
@@ -25,7 +18,7 @@ echo "Run PHP Lint"
 if ! find . -name \*.php ! -path "./.Build/*" 2>/dev/null | parallel --gnu php -d display_errors=stderr -l {} > /dev/null
 then
   echo "There are syntax errors, please check and fix them."
-  exit 1
+  EXIT_CODE=1
 else
   echo "No syntax errors! Great job!"
 fi
@@ -36,28 +29,36 @@ then
   echo "Some files are not compliant to TYPO3 Coding Standards"
   echo "Please fix the files listed above."
   echo "Tip for auto fix: "
-  echo "  composer tests:setup && composer t3:standards:fix"
-  exit 1
+  echo "  TYPO3_VERSION=\"${TYPO3_VERSION}\" composer tests:setup && composer t3:standards:fix"
+  EXIT_CODE=3
 else
   echo "The code is TYPO3 Coding Standards compliant! Great job!"
 fi
 echo -e "\n\n"
 
+echo "Run XML Lint"
 if ! composer tests:lint-xml
 then
-  echo "Some XML files are not valid"
-  echo "Please fix the files listed above"
-  exit 1
+  EXIT_CODE=4
 fi
 
+echo -e "\n\n"
+echo "Run PHPStan analysis"
+if ! composer tests:phpstan
+then
+  # disable fail temporary
+  #EXIT_CODE=7
+  echo "Error during running the PHPStan analysis, please check and fix them."
+  echo "Tip for working on them: "
+  echo "  TYPO3_VERSION=\"${TYPO3_VERSION}\" composer tests:setup && composer tests:phpstan"
+fi
 
 echo -e "\n\n"
 echo "Run unit tests"
-UNIT_BOOTSTRAP="Build/Test/UnitTestsBootstrap.php"
-if ! .Build/bin/phpunit --colors -c Build/Test/UnitTests.xml --bootstrap=$UNIT_BOOTSTRAP --colors --coverage-text
+if ! composer tests:unit -- --coverage-clover=coverage.unit.clover
 then
   echo "Error during running the unit tests please check and fix them"
-  exit 1
+  EXIT_CODE=5
 fi
 
 #
@@ -94,9 +95,10 @@ fi
 
 echo -e "\n\n"
 echo "Run integration tests"
-INTEGRATION_BOOTSTRAP="Build/Test/IntegrationTestsBootstrap.php"
-if ! .Build/bin/phpunit --colors -c Build/Test/IntegrationTests.xml --bootstrap=$INTEGRATION_BOOTSTRAP --colors --coverage-text
+if ! composer tests:integration -- --coverage-clover=coverage.integration.clover
 then
   echo "Error during running the integration tests please check and fix them"
-  exit 1
+  EXIT_CODE=6
 fi
+
+exit $EXIT_CODE
