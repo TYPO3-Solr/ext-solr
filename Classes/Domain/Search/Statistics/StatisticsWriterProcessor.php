789dd2691bad7783063b9d57b9c1763a3a3103e6
Rafael Kähm <rafael.kaehm@dkd.de>
2023-12-04 11:41:50 +0100

    [TASK] Remove unused field `cookie` in tx_solr_statistics
    This PR removes the unused field cookie from the tx_solr_statistics table. The field has never been used in the extension and has most likely been introduced by accident since the initial statistics table structure partly seems to be similar to ext:indexedsearch statistics table (see https://github.com/TYPO3/typo3/blob/TYPO3_4-5/typo3/sysext/indexed_search/ext_tables.sql#L107C2-L107C2).
    
    Ports: https://github.com/TYPO3-Solr/ext-solr/pull/3893

--- Classes/Domain/Search/Statistics/StatisticsWriterProcessor.php
+++ Classes/Domain/Search/Statistics/StatisticsWriterProcessor.php
@@ -102,7 +102,6 @@ class StatisticsWriterProcessor implements SearchResultSetProcessor
             // @extensionScannerIgnoreLine
             'time_processing' => isset($response->debug->timing->process->time) ? $response->debug->timing->process->time : 0,
             'feuser_id' => (int)$TSFE->fe_user->user['uid'],
-            'cookie' => $TSFE->fe_user->id ?? '',
             'ip' => IpAnonymizationUtility::anonymizeIp($this->getUserIp(), $ipMaskLength),
             'page' => (int)$page,
             'keywords' => $keywords,
