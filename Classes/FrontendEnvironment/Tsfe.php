79697063c75f897dc7b67c91c7daba1052f4cb32
Tim Horstmann <t.horstmann@telekom.de>
2024-02-02 12:01:03 +0100

    Check rootline for feGroups from page with extendToSubpages set

--- Classes/FrontendEnvironment/Tsfe.php
+++ Classes/FrontendEnvironment/Tsfe.php
@@ -152,6 +152,20 @@ class Tsfe implements SingletonInterface
                 $userGroups = explode(',', $pageRecord['fe_group']);
             } else {
                 $userGroups = [0, -1];
+
+                // Check rootline for feGroups from page with extendToSubpages set
+                $rootline = BackendUtility::BEgetRootLine($pageId);
+                // remove the current page from the rootline
+                array_shift($rootline);
+                foreach ($rootline as $page) {
+                    // Skip root node, invalid pages and pages which do not define extendToSubpages
+                    if ((int)($page['uid'] ?? 0) <= 0 || !($page['extendToSubpages'] ?? false)) {
+                        continue;
+                    }
+                    $userGroups = explode(',', $page['fe_group']);
+                    // Stop as soon as a page in the rootline has extendToSubpages set
+                    break;
+                }
             }
             $feUser->user = ['uid' => 0, 'username' => '', 'usergroup' => implode(',', $userGroups) ];
             $feUser->fetchGroupData();
