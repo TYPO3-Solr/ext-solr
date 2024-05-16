eda13b2162585469cc88cfbbb47d3e187b95a1d1
Stefan Frömken <123929835+sfroemkenjw@users.noreply.github.com>
2024-05-16 17:06:25 +0200

    Migrate requireJS to ES6. Solr BE Module JS
    * Migrate requireJS to ES6. Solr BE Module JS
    * Add js as file extension for JavaScript modules
    * Repair modal window in index administration module
    
    Relates: #4043

--- Resources/Public/JavaScript/FormModal.js
+++ Resources/Public/JavaScript/FormModal.js
@@ -1,34 +1,30 @@
-define([
-    'jquery',
-    'TYPO3/CMS/Backend/Modal'
-], function($, Modal) {
-    $(document).ready(function() {
+import $ from 'jquery';
+import Modal from '@typo3/backend/modal.js';
 
+$(document).ready(() => {
+  $('.t3js-modal-formsubmit-trigger').click(function(){
+    const element = $(this);
+    const title = element.data('title');
+    const content = element.data('content');
+    let severity = typeof top.TYPO3.Severity[element.data('severity')] !== 'undefined'
+      ? top.TYPO3.Severity[element.data('severity')]
+      : top.TYPO3.Severity.info;
 
-        $('.t3js-modal-formsubmit-trigger').click(function(){
-
-            var element = $(this);
-            var title = element.data('title');
-            var content = element.data('content');
-            var severity = (typeof top.TYPO3.Severity[element.data('severity')] !== 'undefined') ? top.TYPO3.Severity[element.data('severity')] : top.TYPO3.Severity.info;
-
-            Modal.confirm(title, content, severity, [
-                {
-                    text: 'Confirm',
-                    active: true,
-                    trigger: function() {
-                        element.parents('form:first').submit();
-                        Modal.dismiss();
-                    }
-                }, {
-                    text: 'Abort!',
-                    trigger: function() {
-                        Modal.dismiss();
-                    }
-                }
-            ]);
-
-            return false;
-        })
-    });
+    Modal.confirm(title, content, severity, [
+      {
+        text: 'Confirm',
+        active: true,
+        trigger: function() {
+          element.parents('form:first').submit();
+          Modal.dismiss();
+        }
+      }, {
+        text: 'Abort!',
+        trigger: function() {
+          Modal.dismiss();
+        }
+      }
+    ]);
+    return false;
+  });
 });
