f7ad16ae4db60af422efa4df9efd9ff73a60747f
thomashohn <tho@gyldendal.dk>
2024-07-26 10:33:43 +0200

    [FIX] Re-added template variables for SearchFormViewHelper
    This PR re-introduces the template variables in the SearchFormViewHelper
    
    Cherry-Picked from: https://github.com/TYPO3-Solr/ext-solr/commit/8f60143e568e9bb05b030542762719a3d4586480
    Fixes: #4117
    Ports: #4118

--- Classes/ViewHelpers/SearchFormViewHelper.php
+++ Classes/ViewHelpers/SearchFormViewHelper.php
@@ -79,7 +79,7 @@ class SearchFormViewHelper extends AbstractSolrFrontendTagBasedViewHelper
     }
 
     /**
-     * Render search form tag
+     * Renders search form-tag
      *
      * @throws AspectNotFoundException
      * @noinspection PhpMissingReturnTypeInspection
@@ -110,11 +110,31 @@ class SearchFormViewHelper extends AbstractSolrFrontendTagBasedViewHelper
         // @extensionScannerIgnoreLine
         $this->getTemplateVariableContainer()->add('q', $this->getQueryString());
         // @extensionScannerIgnoreLine
+        $this->getTemplateVariableContainer()->add('pageUid', $pageUid);
+        // @extensionScannerIgnoreLine
+        $this->getTemplateVariableContainer()->add(
+            'languageUid',
+            (
+                $this->renderingContext
+                    ->getAttribute(ServerRequestInterface::class)
+                    ->getAttribute('language')
+                    ?->getLanguageId() ?? 0
+            )
+        );
+        // @extensionScannerIgnoreLine
         $this->getTemplateVariableContainer()->add('existingParameters', $this->getExistingSearchParameters());
         // @extensionScannerIgnoreLine
+        // Added addPageAndLanguageId for compatibility
+        $this->getTemplateVariableContainer()->add('addPageAndLanguageId', false);
         $formContent = $this->renderChildren();
         // @extensionScannerIgnoreLine
+        $this->getTemplateVariableContainer()->remove('addPageAndLanguageId');
+        // @extensionScannerIgnoreLine
         $this->getTemplateVariableContainer()->remove('q');
+        // @extensionScannerIgnoreLine
+        $this->getTemplateVariableContainer()->remove('pageUid');
+        // @extensionScannerIgnoreLine
+        $this->getTemplateVariableContainer()->remove('languageUid');
         // @extensionScannerIgnoreLine
         $this->getTemplateVariableContainer()->remove('existingParameters');
 
