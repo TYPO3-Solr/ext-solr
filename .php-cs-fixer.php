01e5387c011ea6cb6a6c4a700df0634f161ad503
Rafael Kähm <rafael.kaehm@dkd.de>
2024-10-07 11:29:39 +0200

    [TASK] Update dependencies
    * solarium/solarium to 6.3.5
    * phpunit/phpunit to 11.4+
    * typo3/coding-standards to v0.8.0
    * typo3/testing-framework to [dev-main#47bf0daab24b4210221f46a1fde59b9c3e245e39](https://github.com/TYPO3/testing-framework/commit/47bf0daab24b4210221f46a1fde59b9c3e245e39)
    
    Relates: #3995

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -13,6 +13,11 @@ if (getenv('IS_ON_GITHUB_ACTIONS') === 'true') {
 $config
     ->addRules(
         [
+            'global_namespace_import' => [
+                'import_classes' => true,
+                'import_constants' => false,
+                'import_functions' => true,
+            ],
             'ordered_imports' => [
                 'imports_order' => ['class', 'function', 'const'],
                 'sort_algorithm' => 'alpha'
