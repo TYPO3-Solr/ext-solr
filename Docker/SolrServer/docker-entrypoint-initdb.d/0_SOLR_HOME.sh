bec3fc2ad6c690ef0d92e16184194630ba4c1873
morph027 <stefan.heitmueller@gmx.com>
2025-06-04 20:37:15 +0200

    only copy when volume is empty
    Signed-off-by: morph027 <stefan.heitmueller@gmx.com>

--- Docker/SolrServer/docker-entrypoint-initdb.d/0_SOLR_HOME.sh
+++ Docker/SolrServer/docker-entrypoint-initdb.d/0_SOLR_HOME.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ "${SOLR_HOME}" != "/var/solr/data" ]; then
-    cp -rv /var/solr/data/* "${SOLR_HOME}/"
+if [ "${SOLR_HOME}" != "/var/solr/data" ] && [ ! -f "${SOLR_HOME}/solr.xml" ]; then
+  cp -rv /var/solr/data/* "${SOLR_HOME}/"
 fi
