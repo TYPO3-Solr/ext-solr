84a632206fb7d4fb17343c2fab0a84d37e22c67a
morph027 <stefan.heitmueller@gmx.com>
2025-06-05 10:25:32 +0200

    adjust disable-cores script for custom SOLR_HOME
    Signed-off-by: morph027 <stefan.heitmueller@gmx.com>

--- Docker/SolrServer/docker-entrypoint-initdb.d/disable-cores.sh
+++ Docker/SolrServer/docker-entrypoint-initdb.d/disable-cores.sh
@@ -4,7 +4,7 @@ shopt -s extglob
 
 if [ -n "${TYPO3_SOLR_ENABLED_CORES}" ]
 then
-  for CORE_TO_DISABLE in /var/solr/data/cores/*/core.properties
+  for CORE_TO_DISABLE in "${SOLR_HOME:-/var/solr/data}"/cores/*/core.properties
   do
     mv "${CORE_TO_DISABLE}" "${CORE_TO_DISABLE}_disabled";
   done
@@ -12,6 +12,6 @@ then
   for CORE_to_ENABLE in ${TYPO3_SOLR_ENABLED_CORES}
   do
     echo "Enable core ${CORE_to_ENABLE}"
-    mv "/var/solr/data/cores/${CORE_to_ENABLE}/core.properties_disabled" "/var/solr/data/cores/${CORE_to_ENABLE}/core.properties"
+    mv "${SOLR_HOME:-/var/solr/data}/cores/${CORE_to_ENABLE}/core.properties_disabled" "${SOLR_HOME:-/var/solr/data}/cores/${CORE_to_ENABLE}/core.properties"
   done
 fi
