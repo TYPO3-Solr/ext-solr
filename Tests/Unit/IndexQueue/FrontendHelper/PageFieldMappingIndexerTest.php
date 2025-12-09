4cfe23ef7fc64e19ce7e3e17b1d8aa9b98480e6d
Rafael Kähm <rafael.kaehm@dkd.de>
2025-12-09 14:03:03 +0100

    !!![FEATURE] allow neted TypoScript on multiValue fields
    This breaking change allows nested TypoScript index configurations for multi-value/array fields like:
    
    ```
    plugin.tx_solr.index.queue.pages.fields.someDoktypeSpecificCategory_stringM = CASE
    plugin.tx_solr.index.queue.pages.fields.someDoktypeSpecificCategory_stringM {
      key.field = doktype
      80 = SOLR_RELATION
      80 {
        localField = some_doktype_specific_sys_category
        multiValue = 1
      }
    }
    ```
    
    See:
    * https://docs.typo3.org/p/apache-solr-for-typo3/solr/main/en-us/Configuration/Reference/TxSolrIndex.html#index-helper-solr-multivalue
    * https://docs.typo3.org/p/apache-solr-for-typo3/solr/main/en-us/Configuration/Reference/TxSolrIndex.html#index-helper-solr-relation
    * https://docs.typo3.org/p/apache-solr-for-typo3/solr/main/en-us/Configuration/Reference/TxSolrIndex.html#solr-classification
    
    This commit removes the SerializedValueDetector hook without any replacements,
    due of [new TypoScript parser in Frontend on TYPO3 12 ](https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97816-NewTypoScriptParserInFrontend.html),
    which does not requiere any manual stdWrap by EXT:solr.
    Each custom cObect implementation returning the array/object as PHP serialized string will be used without registration or check.
    Note: Empty arrays/objects will not be written to the documents.
    
    ---
    
    EXT:solr uses TypoScript `->cObjGetSingle` method for resolving the field values, which returns always the strings.
    For multi-value/array fields EXT:solr provides the custom cObejcts, which utilize the serialisation feature of PHP:
    * `SOLR_CLASSIFICATION`
    * `SOLR_MULTIVALUE`
    * `SOLR_RELATION`
    * or any custom cObject registered via
       https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-96659-RegistrationOfCObjectsViaTYPO3_CONF_VARS.html
       and providing possibility to return the array as serialized string
    Those cObjects serialized return value will be unserialized before setting it as flields value.
    
    The condition for copied TypoScript object `someField < someObject` in
    `\ApacheSolrForTypo3\Solr\IndexQueue\AbstractIndexer::resolveFieldValue` method is not required anymore and was removed in this commit as well,
    because since [ TYPO3 12 refactoring of TypoScript parser](https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-97816-NewTypoScriptParserInFrontend.html),
    TYPO3 config array is always parsed and resolved incl.l refernces and copies of objects, before it is used by EXT:solr.
    
    Relates: #3900
    Ports: #4485

---
+++ Tests/Unit/IndexQueue/FrontendHelper/PageFieldMappingIndexerTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace ApacheSolrForTypo3\Solr\Tests\Unit\IndexQueue\FrontendHelper;
+
+use ApacheSolrForTypo3\Solr\IndexQueue\AbstractIndexer;
+use ApacheSolrForTypo3\Solr\IndexQueue\FrontendHelper\PageFieldMappingIndexer;
+use ApacheSolrForTypo3\Solr\System\Configuration\TypoScriptConfiguration;
+use ApacheSolrForTypo3\Solr\System\Solr\Document\Document;
+use ApacheSolrForTypo3\Solr\Tests\Unit\IndexQueue\AbstractIndexerTest;
+use ApacheSolrForTypo3\Solr\Tests\Unit\SetUpUnitTestCase;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\DependencyInjection\Container;
+use TYPO3\CMS\Core\EventDispatcher\NoopEventDispatcher;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectFactory;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+class PageFieldMappingIndexerTest extends SetUpUnitTestCase
+{
+    protected ContentObjectFactory|MockObject $contentObjectFactoryMock;
+    protected ContentObjectRenderer $contentObjectRenderer;
+    protected MockObject|AbstractContentObject $contentObjectMock;
+
+    protected function setUp(): void
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['solr']['detectSerializedValue'] = [];
+        parent::setUp();
+
+        $container = new Container();
+        $tsfe = $GLOBALS['TSFE'] = $this->getMockBuilder(TypoScriptFrontendController::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $request = $GLOBALS['TYPO3_REQUEST'] = new ServerRequest();
+        $this->contentObjectRenderer = new ContentObjectRenderer($tsfe);
+        $this->contentObjectRenderer->setRequest($request);
+        $this->contentObjectFactoryMock = $this->getMockBuilder(ContentObjectFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->contentObjectMock = $this->createMock(AbstractContentObject::class);
+
+        $this->contentObjectFactoryMock
+            ->expects(self::any())
+            ->method('getContentObject')
+            ->willReturn($this->contentObjectMock);
+
+        $container->set(ContentObjectFactory::class, $this->contentObjectFactoryMock);
+        $container->set(EventDispatcherInterface::class, new NoopEventDispatcher());
+        GeneralUtility::setContainer($container);
+    }
+
+    /**
+     * Test that field values can be resolved
+     */
+    #[DataProviderExternal(AbstractIndexerTest::class, 'indexingDataProvider')]
+    #[Test]
+    public function resolveFieldValue(
+        array $indexingConfiguration,
+        string $solrFieldName,
+        array $data,
+        array $mockSettings,
+        mixed $expectedValue,
+    ): void {
+        $subject = $this->getAccessibleMock(
+            PageFieldMappingIndexer::class,
+            array_merge(
+                [
+                ],
+                $mockSettings[AbstractIndexer::class]['methods'] ?? [],
+            ),
+        );
+        $typoScriptConfiguration = new TypoScriptConfiguration(
+            [
+                'plugin.' => [
+                    'tx_solr.' => [
+                        'index.' => [
+                            'queue.' => [
+                                'pages.' => [
+                                    'fields.' => $indexingConfiguration,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        );
+        $this->inject(
+            $subject,
+            'configuration',
+            $typoScriptConfiguration,
+        );
+        $tsfe = $this->createMock(TypoScriptFrontendController::class);
+        $tsfe->id = 0;
+        if (is_callable($mockSettings['modsCallable'] ?? null)) {
+            $mockSettings['modsCallable']();
+        }
+        $document = new Document();
+        self::assertSame(
+            $expectedValue,
+            $this->callInaccessibleMethod(
+                $subject,
+                'resolveFieldValue',
+                $solrFieldName,
+                $document,
+                $data,
+            ),
+        );
+    }
+}
