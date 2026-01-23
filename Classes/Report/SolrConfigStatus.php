2c911a8977caefc514a94e36759a1cd6869850b1
Rafael Kähm <rafael.kaehm@dkd.de>
2026-01-23 16:19:39 +0100

    [TASK] Prepare schema/configset for dev-14.0.x
    Relates: #4519

--- Classes/Report/SolrConfigStatus.php
+++ Classes/Report/SolrConfigStatus.php
@@ -40,7 +40,7 @@ class SolrConfigStatus extends AbstractSolrStatus
      *
      * Must be updated when changing the solrconfig.
      */
-    public const RECOMMENDED_SOLRCONFIG_VERSION = 'tx_solr-13-1-0--20251024';
+    public const RECOMMENDED_SOLRCONFIG_VERSION = 'tx_solr-14-0-0--20260123';
 
     /**
      * Compiles a collection of solrconfig version checks against each configured
