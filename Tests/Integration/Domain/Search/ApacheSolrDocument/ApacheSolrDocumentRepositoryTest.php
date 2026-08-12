81d9f85ba2bf7362721c0913b80a502ecd42bbbb
Rafael Kähm <rafael.kaehm@dkd.de>
2026-08-12 10:03:33 +0200

    [BUGFIX] PHPStan issues 2026.08.12
    This commit fixes the CI build failures introduced by newer PHPStan versions.

--- Tests/Integration/Domain/Search/ApacheSolrDocument/ApacheSolrDocumentRepositoryTest.php
+++ Tests/Integration/Domain/Search/ApacheSolrDocument/ApacheSolrDocumentRepositoryTest.php
@@ -47,7 +47,6 @@ final class ApacheSolrDocumentRepositoryTest extends IntegrationTestBase
 
         self::assertIsArray($apacheSolrDocumentsCollection, 'Repository did not get Document collection from pageId 3.');
         self::assertNotEmpty($apacheSolrDocumentsCollection, 'Repository did not get apache solr documents from pageId 3.');
-        // @phpstan-ignore staticMethod.alreadyNarrowedType (Document[] is only guaranteed by @return docblock, not the native array return type)
         self::assertInstanceOf(Document::class, $apacheSolrDocumentsCollection[0], 'ApacheSolrDocumentRepository returned not an array of type Document.');
     }
 
